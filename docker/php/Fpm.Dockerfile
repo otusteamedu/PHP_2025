c352a5761f7e91980727c5be355f2e19b9b01ada
Pavel <pavel.klimenko.1989@gmail.com>
2025-02-15 18:29:25 +0300

    downloaded and tested my package

--- docker/php/Fpm.Dockerfile
+++ docker/php/Fpm.Dockerfile
@@ -3,6 +3,8 @@ FROM php:8.3-fpm
 RUN apt-get update && apt-get install -y \
     libzip-dev \
     libpq-dev \
+    zip \
+    unzip \
     && curl -sS https://getcomposer.org/installer | php && \
   mv composer.phar /usr/local/bin/composer
 
--- public/index.php
+++ public/index.php
@@ -1,1 +1,13 @@
-<?php echo 'Hello Docker111rrrrr'; ?>
+<?php 
+
+require_once('../vendor/autoload.php');
+
+use Pavelklimenko\OtusTestPackage\Functions\TestFunctionalClass;
+
+$pavelClass = new TestFunctionalClass();
+
+$sum = $pavelClass->calculateSum(3, 5);
+
+echo $sum; 
+
+?>
