1dfa6de35e55fbbfdb17c365b3ac3dd6afefab96
Максим Дмитриев <makc-dmv@yandex.ru>
2025-02-15 09:48:49 +0500

    Home work № 2: small refactor

--- sum.sh
+++ sum.sh
@@ -5,10 +5,11 @@ if [ "$#" -ne 2 ]; then
     exit 1;
 fi
 
-if ! [[ $1 =~ ^-?[0-9]+(\.[0-9]+)?$ ]] || ! [[ $2 =~ ^-?[0-9]+(\.[0-9]+)?$ ]]; then
+REGEX='^-?[0-9]+(\.[0-9]+)?$';
+
+if ! [[ $1 =~ $REGEX ]] || ! [[ $2 =~ $REGEX ]]; then
     echo 'Arguments must be number!';
     exit 1;
 fi
 
-sum=$(awk "BEGIN {print $1 + $2}" );
-echo "Sum: $sum";
+echo "Sum: $(awk "BEGIN {print $1 + $2}" )";
