23d17f0a7a221ae2dab5c3e118e5aac283ff4812
zibrov <zibrov@nanocad.ru>
2025-02-17 09:27:32 +0300

    понизил версию php и изменил namespace

--- src/index.php
+++ src/index.php
@@ -4,11 +4,17 @@ declare(strict_types=1);
 require_once __DIR__ . '/composer/vendor/autoload.php';
 
 use Zibrov\OtusPhp2025Hw4\Numbers;
-use ZibrovOleg\OtusPhp2025Hw4\Function\Maths;
+use ZibrovOleg\OtusPhp2025Hw4\Maths;
 
-$number = (new Numbers)?->getRandom();
-if ($number) {
-    if (class_exists('ZibrovOleg\OtusPhp2025Hw4\Function\Maths')) {
+$obNumbers = new Numbers();
+try {
+    $number = $obNumbers->getRandom();
+} catch (Exception $e) {
+    echo 'Error numbers: ' . $e->getMessage();
+}
+
+if (!empty($number)) {
+    if (class_exists(Maths::class)) {
         echo 'Fibonacci number "' . $number . '":' . PHP_EOL;
         echo Maths::getFibonacci($number) . PHP_EOL;
     } else {
