70c831dc366fbfb26515ddc4913bc80c71e6fa35
Pavel Klimenko <p.klimenko@itachsoft.com>
2025-02-19 12:33:02 +0300

    Разработано АПИ для проверки строки со скобками. Создана docker сборка (nginx+php-fpm)

--- docker/php/Fpm.Dockerfile
+++ docker/php/Fpm.Dockerfile
@@ -2,8 +2,6 @@ FROM php:8.3-fpm
 
 RUN apt-get update && apt-get install -y \
     libzip-dev \
-    libpq-dev \
-    && curl -sS https://getcomposer.org/installer | php && \
-  mv composer.phar /usr/local/bin/composer
+    libpq-dev
 
 WORKDIR /var/www/app
--- public/index.php
+++ public/index.php
@@ -1,1 +1,52 @@
-<?php echo 'Hello Docker111rrrrr'; ?>
+<?php
+
+try {
+    if (!isset($_POST['string']) || $_POST['string'] == '') {
+        throw new RuntimeException('Не передан или передан пустой параметр "string"');
+    }
+
+    if (!checkBrackets($_POST['string'])) {
+        throw new RuntimeException('Неверное соотношение открытых и закрытых скобок');
+    }
+
+    http_response_code(200);
+    echo json_encode([
+        'string' => $_POST['string'],
+        'status' => 'success',
+        'message' => 'Строка успешно обработана',
+    ]);
+} catch (\Exception $exception) {
+    http_response_code(400);
+    echo json_encode([
+        'string' => $_POST['string'],
+        'status' => 'error',
+        'message' => $exception->getMessage(),
+    ]);
+}
+
+function checkBrackets($str) {
+    // Преобразуем строку в массив символов
+    $chars = str_split($str);
+    $open = 0;
+    $close = 0;
+
+    foreach ($chars as $char) {
+        switch ($char) {
+            case '(':
+                $open++;
+                break;
+            case ')':
+                $close++;
+                break;
+        }
+    }
+
+    $openAndCloseBracketsEqualAmount = $open == $close;
+    $correctPairsOfBrackets = preg_match('/(\([^()]*\)|\{[^{}]*\}|<[^<>]*>|\[[^\[\]]*\]|\(.*\))/', $str) === 1;
+
+    return ($openAndCloseBracketsEqualAmount && $correctPairsOfBrackets);
+}
+
+
+
+
