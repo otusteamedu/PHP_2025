5fc38fa22d14bad78db3af2f97159c5504c5153b
Pavel Klimenko <p.klimenko@itachsoft.com>
2025-02-20 10:42:17 +0300

    Разработан балансировщик. Подключен кластер Redis

--- docker/php/Fpm.Dockerfile
+++ docker/php/Fpm.Dockerfile
@@ -4,4 +4,6 @@ RUN apt-get update && apt-get install -y \
     libzip-dev \
     libpq-dev
 
+#COPY config/php.ini /usr/local/etc/php/
+
 WORKDIR /var/www/app
---
+++ public/test_balancer.php
@@ -0,0 +1,9 @@
+<?php
+
+//https://sergeymukhin.com/blog/kak-ispolzovat-redis-cluster-dlia-keshirovaniia
+
+echo "Запрос обработал контейнер: " . $_SERVER['HOSTNAME'];
+phpinfo();
+
+
+
