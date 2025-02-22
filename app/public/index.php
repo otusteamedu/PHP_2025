75ae0d4cc7645fcecc221de6b00f792dad74bd04
nikolay vanzhin <nikolay.vanzhin@yandex.ru>
2025-02-22 10:57:42 +0500

    feat. 1.1 and 1.2 done

--- app/public/index.php
+++ app/public/index.php
@@ -1,3 +1,23 @@
 <?php
+try {
+    $postParam = 'string';
+    if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+        throw new Exception('Request method must be POST.');
+    }
+    $requestBody = file_get_contents('php://input');
+    preg_match("/$postParam=((?>[^()]|\((?1)\))*)$/m", $requestBody, $matches);
+    if (!current($matches)) {
+        throw new Exception(sprintf('Request body must have the correct format, for example `%s=()()()()`.', $postParam));
+    }
+    if (current($matches) === $postParam . '=') {
+        throw new Exception(sprintf('Parameter `%s` has no value.', $postParam));
+    }
+    http_response_code(200);
 
-phpinfo();
+    echo 'Request body has the correct format.';
+
+} catch (Throwable $e) {
+    http_response_code(400);
+
+    echo $e->getMessage();
+}
