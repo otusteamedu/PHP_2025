6d96e5a11a22d6714e74b06910cf63b57f451bf1
Artem Suchkov Artmin Notebook <artemsuchkov@yandex.ru>
2025-02-23 21:27:07 +0300

    artemsuchkov/hw-1-5

--- src/index.php
+++ src/index.php
@@ -1,47 +1,41 @@
 <? 
 
-session_start();
-
 require_once "vendor/autoload.php";
-
-$redis = new Redis();
-$redis->connect(getenv('REDIS_HOST'), 6379);
 
 $renderHtml = new \MyTestApp\RenderHtml();
 
-if(isset($_POST["string"])) {
+if(isset($_POST["email_list"])) {
     
-    // Если строка не валидна отдаем код 400 и записываем данные в Редис об ошибочной валидации
+    $email_list = explode(PHP_EOL, $_POST["email_list"]);
 
-    if(!\MyTestApp\Common::isValidBrackets($_POST["string"])) {
-        http_response_code(400);
-        $redis->set(session_id(), $redis->get(session_id()). "Запрос обработал контейнер: " . $_SERVER['HOSTNAME']." . Строка \"{$_POST["string"]}\" - ошибка<br/>");
-    }
+    $renderHtml->renderHtml("<h2>Обычная проверка</h2>");
 
-    // Если строка не валидна записываем данные в Редис о верной валидации
-        
-    else 
-        $redis->set(session_id(), $redis->get(session_id()). "Запрос обработал контейнер: " . $_SERVER['HOSTNAME']." . Строка \"{$_POST["string"]}\" - верно<br/>");
+    foreach ($email_list as $email) 
+        if(\MyTestApp\EmailValidation::isValidEmail($email))
+            $renderHtml->renderHtml("<p>$email верный</p>");
+        else 
+        $renderHtml->renderHtml("<p>$email ошибочный</p>");
+            echo "";
+
+    $renderHtml->renderHtml("<h2>Проверка через DNS</h2>");
+
+    foreach ($email_list as $email) 
+        if(\MyTestApp\EmailValidation::isValidEmailThrowDNS($email))
+            $renderHtml->renderHtml("<p>$email верный</p>");
+        else 
+        $renderHtml->renderHtml("<p>$email ошибочный</p>");
+            echo "";
     
         
 }
 
-// Если нет данных с ключем сессии, то создаем запись с этим ключем
-
-if(!$redis->get(session_id()))
-    $redis->set(session_id(), "Запрос обработал контейнер: " . $_SERVER['HOSTNAME'].". Сессия записана в Redis. Ключ: ".session_id()."</br>");
-
-// Выводим данные по ключу сессии (для кластера)
-
-else 
-    $renderHtml->renderHtml($redis->get(session_id()));
-
+$email_list = $_POST["email_list"] ?? 'usermail1@ya.ru'.PHP_EOL.'usermail1@yandex.ru2'.PHP_EOL.'usermail1';
 
 $renderHtml->renderHtml("<hr/>");
 $renderHtml->renderHtml("
 <form method='post'>
-    <input type='text' size='50' name='string' value='' placeholder='Введите строку для проверки' />
-    <input type='submit' value='Проверить'/>
+    <textarea name='email_list' style='width:300px; height:200px;'>{$email_list}</textarea>
+    <p><input type='submit' value='Проверить'/></p>
 </form>
 ");
 
