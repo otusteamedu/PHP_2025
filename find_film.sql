3da76f0a90a3df18ad0dc43f864db71c7127f211
nikolay vanzhin <nikolay.vanzhin@yandex.ru>
2025-03-02 15:08:25 +0500

    fix. hw1-7 done

--- find_film.sql
+++ find_film.sql
@@ -3,5 +3,6 @@ FROM ticket t
          JOIN public.session s on s.id = t.session_id
          JOIN public.film f on f.id = s.film_id
 GROUP BY f.id
-ORDER BY total_sum DESC LIMIT 1
+ORDER BY total_sum DESC
+LIMIT 1
 ;
