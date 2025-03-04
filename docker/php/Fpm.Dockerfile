1efc184398640f7af6859abd5125504de2bc6ae4
Pavel Klimenko <p.klimenko@itachsoft.com>
2025-03-04 11:53:06 +0300

    Создана минимальная docker + xDebug конфигурация

--- docker/php/Fpm.Dockerfile
+++ docker/php/Fpm.Dockerfile
@@ -6,4 +6,6 @@ RUN apt-get update && apt-get install -y \
     && curl -sS https://getcomposer.org/installer | php && \
   mv composer.phar /usr/local/bin/composer
 
+RUN pecl install xdebug && docker-php-ext-enable xdebug
+
 WORKDIR /var/www/app
--- public/index.php
+++ public/index.php
@@ -1,1 +1,29 @@
-<?php echo 'Hello Docker111rrrrr'; ?>
+<?php
+
+/**
+ * Definition for a singly-linked list.
+ * class ListNode {
+ *     public $val = 0;
+ *     public $next = null;
+ *     function __construct($val = 0, $next = null) {
+ *         $this->val = $val;
+ *         $this->next = $next;
+ *     }
+ * }
+ */
+
+//https://leetcode.com/problems/merge-two-sorted-lists/
+
+//class Solution {
+//    /**
+//     * @param ListNode $list1
+//     * @param ListNode $list2
+//     * @return ListNode
+//     */
+//    function mergeTwoLists($list1, $list2) {
+//
+//    }
+//}
+
+$list1 = [1,2,4];
+$list2 = [1,3,4];
