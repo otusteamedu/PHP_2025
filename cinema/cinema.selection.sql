ab1b1edad8f05a494f4840ee98913e94682f535f
Pavel Klimenko <p.klimenko@itachsoft.com>
2025-03-19 13:43:56 +0300

    Добавлены оптимизации. Сформирован excel отчет

--- cinema/cinema.selection.sql
+++ cinema/cinema.selection.sql
@@ -13,10 +13,9 @@ ORDER BY datetime DESC;
 SELECT COUNT(id) AS tickets_purchased_last_week_amount FROM tickets WHERE date_of_purchase > DATE_SUB(CURDATE(), INTERVAL '7' DAY);
 
 /* Поиск 3 самых прибыльных фильмов за неделю */;
-SELECT films.title, SUM(tickets.price) AS total_week_film_profit
+SELECT screening_id, SUM(tickets.price) AS total_week_film_profit
 FROM tickets
-LEFT JOIN screenings ON tickets.screening_id = screenings.id
-LEFT JOIN films ON screenings.film_id = films.id
+RIGHT JOIN screenings ON tickets.screening_id = screenings.id
 WHERE date_of_purchase > DATE_SUB(CURDATE(), INTERVAL '7' DAY)
 GROUP BY screening_id
 ORDER BY total_week_film_profit DESC
--- cinema/optimizations.sql
+++ cinema/optimizations.sql
@@ -1,5 +1,4 @@
 CREATE INDEX idx_date_of_purchase ON tickets (date_of_purchase);
 -- DROP INDEX idx_date_of_purchase ON tickets;
 
-CREATE INDEX idx_tickets_price ON tickets (price);
--- DROP INDEX idx_tickets_price ON tickets;
+ALTER TABLE screenings DROP PRIMARY KEY, ADD PRIMARY KEY (id, datetime) USING BTREE;
