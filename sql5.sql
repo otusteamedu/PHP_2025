15cfd606f1d8f16bf52b0c33628c1553356f5006
Artem Suchkov <artemsuchkov@yandex.ru>
2025-03-19 18:21:59 +0300

    Правки

--- sql5.sql
+++ sql5.sql
@@ -2,11 +2,14 @@
 
 SELECT 
 
-	timetable.time `time`,
-	timetable.hall_id `hall_id`,
-	timetable.film_id `film_id`,
-	halls_seats.seat_id seat_id
+timetable.time `time`,
+timetable.hall_id `hall_id`,
+timetable.film_id `film_id`,
+halls_seats.seat_id seat_id,
+timetable_seatings.hall_seat_id hall_seat_id_buy
 
 FROM `timetable`
 LEFT JOIN halls_seats ON halls_seats.hall_id = timetable.film_id
+LEFT JOIN timetable_seatings ON timetable_seatings.timetable_id = timetable.id AND timetable_seatings.hall_seat_id = halls_seats.id
+
 WHERE timetable.id = 1;
