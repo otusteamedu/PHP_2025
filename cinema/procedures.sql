cad6a540a0ab4bafa070512d690e39379bfd4a95
Pavel Klimenko <p.klimenko@itachsoft.com>
2025-03-20 15:32:44 +0300

    Реализовано создание процедур заполнения таблиц для postgresql

--- cinema/procedures.sql
+++ cinema/procedures.sql
@@ -1,64 +1,62 @@
-DELIMITER //
-CREATE PROCEDURE create_clients(IN max_num INT)
+CREATE OR REPLACE PROCEDURE create_clients(max_num int)AS $$
+    DECLARE
+     counter INT := 0;
 BEGIN
-    DECLARE i INT DEFAULT 1;
-    WHILE i <= max_num
-        DO
-            INSERT INTO clients (fio, phone) VALUES ('Тест Тестовый Тестович', '+11121111111');
-            SET i = i + 1;
-        END WHILE;
-END //
-DELIMITER ;
+    WHILE counter < max_num LOOP
+INSERT INTO clients (fio, phone) VALUES ('Тест Тестовый Тестович', '+11121111111');
+counter := counter + 1;
+END LOOP;
+END
+$$ LANGUAGE 'plpgsql';
 
 
-DELIMITER //
-CREATE PROCEDURE create_films(IN max_num INT)
+CREATE OR REPLACE PROCEDURE create_films(max_num int)AS $$
+DECLARE
+    counter INT := 0;
 BEGIN
-    DECLARE i INT DEFAULT 1;
-    WHILE i <= max_num
-        DO
+    WHILE counter < max_num LOOP
             INSERT INTO films(title, genre_id)
-            VALUES ('Тестовый фильм', (SELECT genres.id FROM genres ORDER BY RAND() LIMIT 1));
-            SET i = i + 1;
-        END WHILE;
-END //
-DELIMITER ;
+            VALUES ('Тестовый фильм', (SELECT genres.id FROM genres ORDER BY random() LIMIT 1));
+            counter := counter + 1;
+        END LOOP;
+END
+$$ LANGUAGE 'plpgsql';
 
-DELIMITER //
-CREATE PROCEDURE create_screenings(IN max_num INT)
+
+
+CREATE OR REPLACE PROCEDURE create_screenings(max_num int)AS $$
+DECLARE
+    counter INT := 0;
 BEGIN
-    DECLARE i INT DEFAULT 1;
-    WHILE i <= max_num
-        DO
-            SET i = i + 1;
+    WHILE counter < max_num LOOP
             INSERT INTO screenings(hall_id, film_id, base_price, datetime) VALUES
-                ((SELECT halls.id FROM halls ORDER BY RAND() LIMIT 1),
-                 (SELECT films.id FROM films ORDER BY RAND() LIMIT 1),
-                 RAND()*(20-10)+10,
-                 DATE_ADD(NOW(), INTERVAL i SECOND));
-        END WHILE;
-END //
-DELIMITER ;
+                ((SELECT halls.id FROM halls ORDER BY random() LIMIT 1),
+                 (SELECT films.id FROM films ORDER BY random() LIMIT 1),
+                 CEILING(RANDOM() * 20),
+                 NOW() + interval '1 second'
+                );
+            counter := counter + 1;
+        END LOOP;
+END
+$$ LANGUAGE 'plpgsql';
 
 
-DELIMITER //
-CREATE PROCEDURE create_tickets(IN max_num INT)
+
+CREATE OR REPLACE PROCEDURE create_tickets(max_num int)AS $$
+DECLARE
+    counter INT := 0;
 BEGIN
-    DECLARE i INT DEFAULT 1;
-    WHILE i <= max_num
-        DO
+    WHILE counter < max_num LOOP
             INSERT INTO tickets(screening_id, client_id, line,place,price,date_of_purchase) VALUES
                 (
-                    (SELECT screenings.id FROM screenings ORDER BY RAND() LIMIT 1),
-                    (SELECT clients.id FROM clients ORDER BY RAND() LIMIT 1),
-                    RAND()*(3-1)+1,
-                    RAND()*(10-1)+1,
-                    RAND()*(20-10)+10,
+                    (SELECT screenings.id FROM screenings ORDER BY random() LIMIT 1),
+                    (SELECT clients.id FROM clients ORDER BY random() LIMIT 1),
+                    CEILING(RANDOM() * 3),
+                    CEILING(RANDOM() * 10),
+                    CEILING(RANDOM() * 20),
                     NOW()
                 );
-
-            SET i = i + 1;
-        END WHILE;
-END //
-DELIMITER ;
-
+            counter := counter + 1;
+        END LOOP;
+END
+$$ LANGUAGE 'plpgsql';
