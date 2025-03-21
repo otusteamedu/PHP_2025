720db33a8d599a9b689c332c8090cef902b50a6e
Pavel Klimenko <p.klimenko@itachsoft.com>
2025-03-21 12:50:45 +0300

    Добавлен xDebug

--- public/index.php
+++ public/index.php
@@ -16,3 +16,28 @@
 //
 
 echo 'test';
+
+require '../vendor/autoload.php';
+
+$client = Elastic\Elasticsearch\ClientBuilder::create()->build();
+
+$params = ['body' => []];
+
+for ($i = 100; $i <= 110; $i++) {
+    $params['body'][] = [
+        'index' => [
+            '_index' => 'my_new_index',
+            '_id'    => $i
+        ]
+    ];
+
+    $params['body'][] = [
+        'title'     => 'test'.$i,
+        'second_field' => 'some more values'
+    ];
+}
+
+// Send the last batch if it exists
+if (!empty($params['body'])) {
+    $responses = $client->bulk($params);
+}
