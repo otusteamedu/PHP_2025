f3a661027855d51309790277ae7c20f76c3bcede
nikolay vanzhin <nikolay.vanzhin@yandex.ru>
2025-03-23 18:56:19 +0500

    feat. insert.sql updated

--- insert.sql
+++ insert.sql
@@ -31,13 +31,19 @@ $$
         start_datetime   timestamp;
         end_datetime     timestamp;
         current_datetime timestamp;
-        rating           record;
-        film             record;
-        minutes          varchar;
+        rating
+                         record;
+        film
+                         record;
+        minutes
+                         varchar;
     BEGIN
-        start_datetime := timestamp '2025-03-01 09:00:00';
-        end_datetime := start_datetime + INTERVAL '10 days';
-        WHILE end_datetime >= start_datetime
+        start_datetime
+            := timestamp '2025-03-01 09:00:00';
+        end_datetime
+            := start_datetime + INTERVAL '10 days';
+        WHILE
+            end_datetime >= start_datetime
             LOOP
                 FOR cinema_room IN
                     SELECT *
@@ -46,17 +52,57 @@ $$
                         start_datetime := start_datetime;
                         current_datetime := start_datetime;
                         LOOP
-                            SELECT * INTO film FROM film ORDER BY RANDOM() LIMIT 1;
-                            SELECT * INTO rating FROM session_rating ORDER BY RANDOM() LIMIT 1;
+                            SELECT *
+                            INTO film
+                            FROM film
+                            ORDER BY RANDOM()
+                            LIMIT 1;
+                            SELECT *
+                            INTO rating
+                            FROM session_rating
+                            ORDER BY RANDOM()
+                            LIMIT 1;
                             minutes := round(film.duration / 60, 0) + 30 || ' minutes';
                             INSERT INTO public.session (id, start_from, end_to, film_id, rating_id, cinema_room_id)
                             VALUES (gen_random_uuid(), current_datetime, current_datetime + minutes::interval, film.id,
                                     rating.id, cinema_room.id);
                             current_datetime := current_datetime + minutes::interval + interval '15 minutes';
-                            EXIT WHEN current_datetime >= start_datetime + INTERVAL '15 hours';
+                            EXIT
+                                WHEN current_datetime >= start_datetime + INTERVAL '15 hours';
                         END LOOP;
                     END LOOP;
                 start_datetime := start_datetime + INTERVAL '1 day';
             END LOOP;
     END;
 $$;
+
+-- таблица мест
+DO
+$$
+    DECLARE
+        cinema_room record;
+        rows        integer;
+        seats       integer;
+    BEGIN
+        FOR cinema_room IN
+            SELECT *
+            FROM public.cinema_room
+            LOOP
+                rows := random(3, 15);
+                seats := random(10, 20);
+                FOR r IN 1..rows
+                    LOOP
+                        FOR s IN 1..seats
+                            LOOP
+                                INSERT INTO cinema_room_seat (id, row, place, cinema_room_id, rating)
+                                VALUES (gen_random_uuid(), r, s, cinema_room.id, random() + 1);
+
+                            END LOOP;
+                    END LOOP;
+            END LOOP;
+    END;
+$$;
+
+
+
+
