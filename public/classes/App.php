6c2c1ce1c6d4db05059e28a2c699c48f74643596
Pavel <pavel.klimenko.1989@gmail.com>
2025-03-30 23:11:48 +0300

    удален тестовый код

--- public/classes/App.php
+++ public/classes/App.php
@@ -50,29 +50,3 @@ class App
     }
 
 }
-
-function pr_debug($var)
-{
-    //if ($_REQUEST['deb'] == 'Y') {
-    $bt = debug_backtrace();
-    $bt = $bt[0];
-    ?>
-    <div style='font-size:9pt; color:#000; background:#fff; border:1px dashed #000;'>
-        <div style='padding:3px 5px; background:#99CCFF; font-weight:bold;'>File: <?= $bt["file"] ?>
-            [<?= $bt["line"] ?>]
-        </div>
-        <?
-        if ($var === 0) {
-            echo '<pre>пусто</pre>';
-            var_dump($var);
-        } else {
-            echo '<pre>';
-            print_r($var);
-            echo '</pre>';
-        }
-        ?>
-    </div>
-    <?
-    //exit();
-    //}
-}
