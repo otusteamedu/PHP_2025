6a29b023b5de5841ff1138a20005bc3799373593
Artem Suchkov Artmin Notebook <artemsuchkov@yandex.ru>
2025-04-06 16:25:05 +0300

    переписал класс connect

--- src/classes/MyApp.php
+++ src/classes/MyApp.php
@@ -4,14 +4,11 @@ namespace MyTestApp;
 
 Class MyApp {
 
-    public $render = "";
-
     public function __construct() {
         
         $redis = (new \MyTestApp\Commands\Redis\Connect);
         $MethodDispatcher = new \MyTestApp\MethodDispatcher($redis); 
-        $renderHtml = new \MyTestApp\RenderHtml("views/page.php",["answer"=>$MethodDispatcher->answer]);
-        //$this->render = $renderHtml->html;
+        new \MyTestApp\RenderHtml("views/page.php",["answer"=>$MethodDispatcher->answer]);
 
     }
 
