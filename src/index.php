ee62fd5d97220ca0a3ab71687e70377e74d4408c
Artem Suchkov Artmin Notebook <artemsuchkov@yandex.ru>
2025-05-05 16:54:07 +0300

    artemsuchkov/hw-2-9

--- src/index.php
+++ src/index.php
@@ -3,28 +3,6 @@
 //require_once "vendor/autoload.php";
 spl_autoload_register();
 
-use Infrastructure\Rest\Common;
-
-switch (Common::get_method()) {
-
-    case "POST":
-        $api = new Infrastructure\Rest\Post\MethodPost(Common::get_endpoint(),Common::get_request_data());
-		$api->endpoint();
-        break;
-    
-    case "GET":
-        $api = new Infrastructure\Rest\Get\MethodGet(Common::get_endpoint(),Common::get_request_data());
-        $api->endpoint();
-        break;
-
-	default:
-		Common::send_response(array(
-			'code' => 405,
-			'status' => 'failed',
-			'message' => 'Method not allowed'
-		), 405);
-}
-
 
 
 
