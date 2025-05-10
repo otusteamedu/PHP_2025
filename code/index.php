768101d49777713bfc5b22458b35b4bbf751da23
AlexSamojlo <121355372+AlexSamojlo@users.noreply.github.com>
2025-05-10 16:45:37 +0300

    Update index.php
    Добавил запуск bash-скриптов для второго дз

--- code/index.php
+++ code/index.php
@@ -1,35 +1,10 @@
 <?php
+// Запуск скрипта для суммирования
+echo "<h2>Результат суммирования (Первое задание):</h2>";
+$output = shell_exec('bash /data/mysite.local/sum.sh 5.5 -10');
+echo "<pre>$output</pre>";
 
-// Настройки подключения к базе данных
-$servername = "db"; // Имя сервиса из docker-compose
-$username = "user"; // Имя пользователя, указанное в docker-compose
-$password = "user_password"; // Пароль, указанный в docker-compose
-$dbname = "my_database"; // Имя базы данных, указанное в docker-compose
-
-// Создание подключения
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Проверка подключения
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-echo "Connected successfully";
-
-// Выполнение простого запроса
-$sql = "SELECT DATABASE() AS current_database";
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-    // Вывод текущей базы данных
-    $row = $result->fetch_assoc();
-    echo "Current database: " . $row['current_database'];
-} else {
-    echo "No results found.";
-}
-
-// Закрытие подключения
-$conn->close();
-
-
-
-phpinfo();
+// Запуск скрипта для популярных городов
+echo "<h2>3 наиболее популярных города (Второе задание):</h2>";
+$output = shell_exec('bash /data/mysite.local/popular_cities.sh');
+echo "<pre>$output</pre>";
