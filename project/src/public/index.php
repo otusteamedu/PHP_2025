9217c5e509780a838b036e95edf15ee154908af9
trijin <trijin@gmail.com>
2025-05-11 13:00:16 +0300

    define

--- project/src/public/index.php
+++ project/src/public/index.php
@@ -1,10 +1,10 @@
 <?php
 
-const $appDir = dirname(__DIR__).'/app';
+define("APP_DIR", dirname(__DIR__) . '/app');
 
-require_once $appDir.'/app.php';
-require_once $appDir.'/validator.php';
-require_once $appDir.'/view.php';
+require_once APP_DIR.'/app.php';
+require_once APP_DIR.'/validator.php';
+require_once APP_DIR.'/view.php';
 
 // Создаем экземпляр приложения и запускаем его
 $app = new App();
