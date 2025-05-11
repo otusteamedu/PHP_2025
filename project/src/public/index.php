72a17d40dd716918f9a72cb890d8440f5d9bd981
trijin <trijin@gmail.com>
2025-05-11 12:19:13 +0300

    работа по комментариям index

--- project/src/public/index.php
+++ project/src/public/index.php
@@ -1,34 +1,11 @@
 <?php
 
-function validateLength($str) {
-    if(empty($str)) {
-        throw new Exception("string is required");
-    }
-}
-function validateBrackers($str) {
-    validateLength($str);
-    $openBreackers=0;
-    for ($i=0; $i < strlen($str); $i++) {
-        if($str[$i] == '(') {
-            $openBreackers++;
-        } elseif($str[$i] == ')') {
-            $openBreackers--;
-        }
+const $appDir = dirname(__DIR__).'/app';
 
-        if($openBreackers < 0) {
-            throw new Exception("Wrong syntax on $i");
-        }
-    }
-    if($openBreackers != 0) {
-        throw new Exception("Wrong syntax on $i");
-    }
+require_once $appDir.'/app.php';
+require_once $appDir.'/validator.php';
+require_once $appDir.'/view.php';
 
-}
-
-try {
-    validateBrackers($_POST['string']??'');
-    echo '<code>' . $_POST['string'] . '</code> - right syntax';
-} catch (Exception $e) {
-    http_response_code(400);
-    echo $e->getMessage();
-}
+// Создаем экземпляр приложения и запускаем его
+$app = new App();
+echo $app->run();
