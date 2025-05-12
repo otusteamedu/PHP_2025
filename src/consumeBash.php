4e6ed14acf67eb9a9d78de589851c11abaf024e0
Sobolos <germansobol@yandex.ru>
2025-05-12 23:13:40 +0300

    consume

--- src/consumeBash.php
+++ src/consumeBash.php
@@ -3,4 +3,4 @@
 $number1 = $_POST['number1'];
 $number2 = $_POST['number2'];
 
-echo shell_exec('./consume.bash' . ' ' . $number1 . ' ' . $number2);
+echo shell_exec('bash consume.bash' . ' ' . $number1 . ' ' . $number2);
