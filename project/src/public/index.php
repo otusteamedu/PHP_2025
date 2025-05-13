b3a249af8e09cff4f7fd1db998e837871642bf19
trijin <trijin@gmail.com>
2025-05-13 20:24:12 +0300

    Update index.php
    Когда не нравятся 3 Require в index

--- project/src/public/index.php
+++ project/src/public/index.php
@@ -3,8 +3,6 @@
 define("APP_DIR", dirname(__DIR__) . '/app');
 
 require_once APP_DIR.'/app.php';
-require_once APP_DIR.'/validator.php';
-require_once APP_DIR.'/view.php';
 
 // Создаем экземпляр приложения и запускаем его
 $app = new App();
