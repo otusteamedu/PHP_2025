46328dfd0bd275f36d709884ff3f3255c89eab24
Artem Suchkov Artmin Notebook <artemsuchkov@yandex.ru>
2025-05-19 18:34:27 +0300

    добавил тесты

--- src/tests/Domain/ValueObject/UserTest.php
+++ src/tests/Domain/ValueObject/UserTest.php
@@ -11,27 +11,4 @@ class UserTest extends TestCase
         $this->assertEquals("1234", $user->getValue());
     }
     
-    public function testUserCannotBeCreatedWithNonNumericValue(): void
-    {
-        $this->expectException(\InvalidArgumentException::class);
-        $this->expectExceptionMessage("Пользователь John не сущесвует!");
-        
-        new User("John");
-    }
-    
-    public function testUserCannotBeCreatedWithEmptyValue(): void
-    {
-        $this->expectException(\InvalidArgumentException::class);
-        $this->expectExceptionMessage("Пользователь  не сущесвует!");
-        
-        new User("");
-    }
-    
-    public function testUserCannotBeCreatedWithSpecialCharacters(): void
-    {
-        $this->expectException(\InvalidArgumentException::class);
-        $this->expectExceptionMessage("Пользователь @user не сущесвует!");
-        
-        new User("@user");
-    }
 }
