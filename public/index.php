af103e93061042515063bc40e363747edf6d8b7e
Рахманова Елизавета <you@example.com>
2025-05-21 23:08:16 +0500

    ERachmanova/hw1-1 | Исправлены хосты и удалены наружные порты для редиса и мемкэша

--- public/index.php
+++ public/index.php
@@ -4,19 +4,20 @@ require  __DIR__ . '/../vendor/autoload.php';
 try {
     $dotenv = Dotenv\Dotenv::createImmutable('/app/');
     $dotenv->load();
-    $host = "app_db";
+    $host = $_ENV['DB_HOST'];
     $port = $_ENV['DB_PORT'];
     $dbname = $_ENV["POSTGRES_DB"];
     $username = $_ENV["POSTGRES_USER"];
     $password = $_ENV["POSTGRES_PASSWORD"];
     $connection = new PDO("pgsql:host=$host;port=$port;dbname=$dbname;user=$username;password=$password");
     $memcached = new Memcached();
-    $memcached->addServer('memcached', 11211);
+    $memcached->addServer($_ENV['MEMCACHED_HOST'], 11211);
     $redis = new Redis();
-    $redis->connect('app_redis', 6379);
+    $redis->connect($_ENV['REDIS_HOST'], 6379);
 
     echo "Connected successfully";
 } catch (\Throwable $e) {
+    dump($e->getTraceAsString());
     die("Connection failed: " . $e->getMessage());
 }
 
