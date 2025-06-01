0aba467304d99c548baa13377bbdd96a2e431b0a
Andrey Yurchuk <r1430@tut.by>
2025-06-01 08:54:37 +0300

    refactor: simplify controller instantiation in index.php

--- public/index.php
+++ public/index.php
@@ -5,7 +5,4 @@ use App\Validator\Email\EmailValidator;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$controller = new EmailValidationController(new EmailValidator());
-$response   = $controller->handle();
-
-echo $response;
+echo new EmailValidationController(new EmailValidator())->handle();
