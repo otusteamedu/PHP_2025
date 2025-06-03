2ff7b7171af50ac1eb6ec4cd67312624359d0a4b
Голованов Сергей <golovanovsv16071975@yandex.ru>
2025-06-03 16:13:19 +0500

    PHP WebServers (web-app + nginx balancer)

--- app/web/public/index.php
+++ app/web/public/index.php
@@ -1,3 +1,13 @@
 <?php
 
-phpinfo();
+echo "Запрос обработал контейнер: " . $_SERVER["HOSTNAME"] . PHP_EOL;
+
+$redis = new Redis();
+try {
+  $redis->connect('redis', 6379);
+  $redis->auth($_ENV['REDIS_PASSWORD']);
+  $redis->publish('hello', 'Hello World!');
+  $redis->close();
+} catch (Exception $e) {
+  echo $e->getMessage();
+}
