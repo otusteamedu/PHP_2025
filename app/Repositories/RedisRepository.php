bdd569620a1c945932dd7a1db05f75c2812c038d
fav <a.fenin@soglasie.ru>
2025-06-13 12:59:32 +0300

    hw11 Добавил изменения исходя ДЗ: Анализ кода

--- app/Repositories/RedisRepository.php
+++ app/Repositories/RedisRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repositories;
 
 use App\DTO\EventDTO;
+use App\Services\RedisService;
 use App\Tasks\GetEventHashTask;
 use Exception;
 use Redis;
@@ -14,31 +15,10 @@ class RedisRepository extends Repository
     public Redis $client;
 
     /**
-     * @throws RedisException
      * @throws Exception
      */
     public function __construct() {
-        $this->connect();
-    }
-
-    /**
-     * @return void
-     * @throws Exception
-     */
-    protected function connect(): void {
-        $host = getenv('REDIS_HOST');
-        $port = 6379;
-        $password = getenv('REDIS_PASSWORD');
-
-        try {
-            $redis = new Redis();
-            $redis->connect($host, $port);
-            $redis->auth($password);
-        } catch (RedisException $e) {
-            throw new Exception($e);
-        }
-
-        $this->client = $redis;
+        $this->client = (new RedisService())->client;
     }
 
     /**
