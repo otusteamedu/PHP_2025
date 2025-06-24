bbc9556ce4dcaa4d262f17f03b0ca92aaf31e4b1
Марина Лубник <m.kalinina@21vek.by>
2025-06-24 14:24:49 +0300

    Дз 6

--- cinema.sql
+++ cinema.sql
@@ -6,14 +6,13 @@ CREATE TABLE halls
 (
     id   INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(255) UNIQUE   NOT NULL,
-    type ENUM ('small', 'big') NOT NULL
 );
 
-INSERT INTO halls (name, type)
-VALUES ('Small Hall', 'small'),
-       ('Big Hall', 'big');
+INSERT INTO halls (name)
+VALUES ('hall_1'),
+       ('hall_2');
 
-create table hall_scheme_small
+create table hall_scheme_1
 (
     place   INT,
     line    INT,
@@ -24,14 +23,14 @@ create table hall_scheme_small
     FOREIGN KEY (hall_id) REFERENCES halls (id) ON DELETE CASCADE
 );
 
-INSERT INTO hall_scheme_small (place, line, type, price, hall_id)
+INSERT INTO hall_scheme_1 (place, line, type, price, hall_id)
 VALUES (1, 1, 'Standard', 10.00, 1),
        (2, 1, 'Standard', 10.00, 1),
        (3, 1, 'VIP', 15.00, 1),
        (1, 2, 'Standard', 10.00, 1),
        (2, 2, 'VIP', 15.00, 1);
 
-create table hall_scheme_big
+create table hall_scheme_2
 (
     place   INT,
     line    INT,
@@ -42,7 +41,7 @@ create table hall_scheme_big
     FOREIGN KEY (hall_id) REFERENCES halls (id) ON DELETE CASCADE
 );
 
-INSERT INTO hall_scheme_big (place, line, type, price, hall_id)
+INSERT INTO hall_scheme_2 (place, line, type, price, hall_id)
 VALUES (1, 1, 'Standard', 12.00, 2),
        (2, 1, 'Standard', 12.00, 2),
        (3, 1, 'VIP', 18.00, 2),
