ede18707dbac17ba32c630bff1b47772abc890f9
Konstantin D <merty.doc@gmail.com>
2025-06-28 14:27:32 +0300

    init hw15

--- public/index.php
+++ public/index.php
@@ -1,18 +1,8 @@
 <?php declare(strict_types=1);
 
 use App\App;
-use App\Http\Response;
 
 require(__DIR__ . '/../vendor/autoload.php');
 
-try {
-    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
-    $dotenv->load();
-
-    $app = new App();
-    $response = $app->run();
-
-    echo $response;
-} catch (\Throwable $e) {
-    echo (new Response())->send(500, ['error' => 'Internal server error']);
-}
+$app = new App();
+echo $app->run();
