8a39a45bd02f063077d2ff39f9d4ae84bd3da354
elisad5791 <elisad5791@mail.ru>
2025-06-28 14:14:20 +0500

    Add mongodb connection

--- public/index.php
+++ public/index.php
@@ -1,5 +1,5 @@
 <?php
-use Psr\Http\Message\ResponseInterface as Response;
+/*use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 use App\Controllers\HomeController;
@@ -15,4 +15,24 @@ $app->post('/api/clear', [HomeController::class, 'clear']);
 $app->get('/api/event-conditions', [HomeController::class, 'showEventConditions']);
 $app->get('/api/event-priorities', [HomeController::class, 'showEventPriorities']);
 
-$app->run();
+$app->run();*/
+
+/*$mongoClient = new MongoDB\Client('mongodb://root:example@mongo:27017');
+echo 'here';*/
+
+try {
+    // 1. Подключение к MongoDB
+    $manager = new MongoDB\Driver\Manager('mongodb://root:example@mongo:27017');
+    
+    $query = new MongoDB\Driver\Query([]); // Пустой фильтр = все документы
+    $cursor = $manager->executeQuery('test_database.users', $query);
+
+    echo '<pre>';
+    foreach ($cursor as $doc) {
+        print_r($doc);
+    }
+    echo '</pre>';
+    
+} catch (Exception $e) {
+    echo "Ошибка MongoDB: " . $e->getMessage();
+}
