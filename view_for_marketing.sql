5eb9a32ea7b775af99b7200c7a8c44a47d0a846d
AlexSamojlo <121355372+AlexSamojlo@users.noreply.github.com>
2025-06-29 16:04:59 +0300

    Update view_for_marketing.sql

--- view_for_marketing.sql
+++ view_for_marketing.sql
@@ -2,7 +2,7 @@ CREATE VIEW marketing_data AS
 SELECT f.title AS film, 
        at.type_name AS attribute_type, 
        a.attribute_name AS attribute, 
-       COALESCE(v.value_text, v.value_date::TEXT, v.value_boolean::TEXT) AS value
+       COALESCE(v.value_text, v.value_date::TEXT, v.value_boolean::TEXT, v.value_float::TEXT, v.value_int::TEXT) AS value
 FROM films f
 JOIN values v ON f.film_id = v.film_id
 JOIN attributes a ON v.attribute_id = a.attribute_id
