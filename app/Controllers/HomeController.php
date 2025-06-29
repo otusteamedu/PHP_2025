00412742a089c34bfbf61297813ea17393894836
elisad5791 <elisad5791@mail.ru>
2025-06-29 21:22:22 +0500

    Add services

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -1,27 +1,29 @@
 <?php
 namespace App\Controllers;
 
+use App\Services\MongoService;
+use App\Services\RedisService;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
 class HomeController
 {
-    private $redis;
+    private $service;
 
     public function __construct()
     {
-        $this->redis = new \Redis();
-        $this->redis->connect('redis', 6379);
+        $driver = getenv('DRIVER');
+        $this->service = $driver == 'redis' ? new RedisService() : new MongoService();
     }
 
-    public function add(Request $request, Response $response, $args): Response
+    public function add(Request $request, Response $response, $args)
     {
         $body = file_get_contents('php://input');
         $data = json_decode($body, true);
 
         $event = (string) $data['event'] ?? '';
         $priority = (int) $data['priority'] ?? 0;
-        $conditions = (array) $data['conditions'];
+        $conditions = (array) $data['conditions'] ?? [];
         ksort($conditions);
 
         if (empty($event) || empty($priority) || empty($conditions)) {
@@ -29,20 +31,13 @@ class HomeController
             return $response;
         }
 
-        $conditions1 = [];
-        foreach ($conditions as $key => $val) {
-            $conditions1[] = $key . ':' . $val;
-        }
-        $conditionStr = implode('::', $conditions1);
-
-        $this->redis->hSet('events', $event, $conditionStr);
-        $this->redis->zAdd('priorities', $priority, $event);
-
-        $response->getBody()->write('Событие добавлено в систему');
+        $result = $this->service->add($event, $priority, $conditions);
+        
+        $response->getBody()->write($result);
         return $response;
     }
 
-    public function answer(Request $request, Response $response, $args): Response
+    public function answer(Request $request, Response $response, $args)
     {
         $body = file_get_contents('php://input');
         $data = json_decode($body, true);
@@ -52,69 +47,26 @@ class HomeController
             $response->getBody()->write('Некорректный запрос');
             return $response;
         }
-
         ksort($parameters);
-        $params = [];
-        foreach ($parameters as $par => $val) {
-            $params[] = "$par:$val";
-        }
 
-        $events = $this->redis->hGetAll('events');
-        $rightEvents = [];
-        foreach ($events as $ev => $conditions) {
-            $condArr = explode('::', $conditions);
-            $res = true;
-            foreach ($params as $par) {
-                if (!in_array($par, $condArr)) {
-                    $res = false;
-                    break;
-                }
-            }
-            if ($res) {
-                $rightEvents[] = $ev;
-            }
-        }
+        $message = $this->service->answer($parameters);
 
-        if (empty($rightEvents)) {
-            $response->getBody()->write('Нет событий');
-            return $response;
-        }
-
-        $event = '';
-        $currentPriority = 0;
-        foreach ($rightEvents as $ev) {
-            $priority = $this->redis->zScore('priorities', $ev);
-            if ($priority > $currentPriority) {
-                $currentPriority = $priority;
-                $event = $ev;
-            }
-        }
-
-        $response->getBody()->write($event);
-        return $response;
-    }
-
-    public function showEventConditions(Request $request, Response $response, $args)
-    {
-        $events = $this->redis->hGetAll('events');
-        $message = !empty($events) ? json_encode($events) : 'Нет зарегистрированных событий';
         $response->getBody()->write($message);
         return $response;
     }
 
-    public function showEventPriorities(Request $request, Response $response, $args)
+    public function events(Request $request, Response $response, $args)
     {
-        $events = $this->redis->zRevRange('priorities', 0, -1, ['WITHSCORES' => true]);
-        $message = !empty($events) ? json_encode($events) : 'Нет зарегистрированных событий';
+        $message = $this->service->getEvents();
         $response->getBody()->write($message);
         return $response;
     }
 
     public function clear(Request $request, Response $response, $args)
     {
-        $this->redis->del('events');
-        $this->redis->del('priorities');
-        $response->getBody()->write('Хранилище событий очищено');
+        $result = $this->service->clear();
+        $message = $result ? 'Хранилище событий очищено' : 'Не удалось очистить хранилище';
+        $response->getBody()->write($message);
         return $response;
     }
 }
--- public/index.php
+++ public/index.php
@@ -1,18 +1,15 @@
 <?php
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
-use App\Controllers\MongoController;
+use App\Controllers\HomeController;
 
 require __DIR__ . '/../vendor/autoload.php';
 
 $app = AppFactory::create();
 
-$app->post('/api/add', [MongoController::class, 'add']);
-$app->post('/api/answer', [MongoController::class, 'answer']);
-$app->post('/api/clear', [MongoController::class, 'clear']);
+$app->post('/api/add', [HomeController::class, 'add']);
+$app->post('/api/answer', [HomeController::class, 'answer']);
+$app->post('/api/clear', [HomeController::class, 'clear']);
 
-$app->get('/api/event-conditions', [MongoController::class, 'showEventConditions']);
-$app->get('/api/event-priorities', [MongoController::class, 'showEventPriorities']);
+$app->get('/api/events', [HomeController::class, 'events']);
 
 $app->run();
