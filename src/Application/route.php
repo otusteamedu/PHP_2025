edab8a3fff97fe233a18a7e887d4b6473b376476
fav <a.fenin@soglasie.ru>
2025-07-07 07:58:36 +0300

    hw20 Поменял пути к эндпоинтам, добавил инструкцию

--- src/Application/route.php
+++ src/Application/route.php
@@ -3,7 +3,7 @@
 use Application\Routing\Route;
 use Infrastructure\Http\Controllers\EventController;
 
-Route::get('event', ['controller' => EventController::class, 'method' => 'get']);
-Route::get('event/one', ['controller' => EventController::class, 'method' => 'one']);
-Route::post('event', ['controller' => EventController::class, 'method' => 'create']);
-Route::delete('event', ['controller' => EventController::class, 'method' => 'delete']);
+Route::get('api/v1/event', ['controller' => EventController::class, 'method' => 'get']);
+Route::get('api/v1/event/one', ['controller' => EventController::class, 'method' => 'one']);
+Route::post('api/v1/event', ['controller' => EventController::class, 'method' => 'create']);
+Route::delete('api/v1/event', ['controller' => EventController::class, 'method' => 'delete']);
