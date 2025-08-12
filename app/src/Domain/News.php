9ffa9d25977a58cfb086105bce53895864b92f45
Марина Лубник <m.kalinina@21vek.by>
2025-08-12 03:28:11 +0300

    fix

--- app/src/Domain/News.php
+++ app/src/Domain/News.php
@@ -4,37 +4,19 @@ declare(strict_types=1);
 
 namespace App\Domain;
 
-use App\Domain\Repository\NewsRepositoryInterface;
-use App\Infrastructure\Persistence\Doctrine\ORM\Repository\NewsRepository;
 use DateTimeImmutable;
-use Doctrine\DBAL\Types\Types;
-use Doctrine\ORM\Mapping as ORM;
 
-#[ORM\Table(name: 'news')]
-#[ORM\Entity(repositoryClass: NewsRepository::class)]
 class News
 {
-    #[ORM\Id]
-    #[ORM\GeneratedValue(strategy: 'AUTO')]
-    #[ORM\Column(type: 'integer', unique: true)]
     private ?int $id = null;
 
-    #[ORM\Column(type: Types::TEXT, nullable: false)]
-    private string $url;
-
-    #[ORM\Column(type: Types::TEXT, nullable: true)]
-    private string $title;
-
-    #[ORM\Column(type: Types::DATETIME_IMMUTABLE, nullable: false)]
-    private ?\DateTimeImmutable $createdAt;
+    private DateTimeImmutable $createdAt;
 
     public function __construct(
         string $url,
         string $title,
     )
     {
-        $this->url = $url;
-        $this->title = $title;
         $this->createdAt = new DateTimeImmutable();
     }
 
--- app/src/Infrastructure/Persistence/Doctrine/ORM/Repository/NewsRepository.php
+++ app/src/Infrastructure/Persistence/Doctrine/ORM/Repository/NewsRepository.php
@@ -4,17 +4,18 @@ declare(strict_types=1);
 
 namespace App\Infrastructure\Persistence\Doctrine\ORM\Repository;
 
-use App\Domain\News;
 use App\Domain\Repository\NewsRepositoryInterface;
+use App\Infrastructure\Persistence\Doctrine\ORM\Entity\News as EntityNews;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ManagerRegistry;
+use App\Domain\News;
 
 class NewsRepository extends ServiceEntityRepository implements NewsRepositoryInterface
 {
     public function __construct(ManagerRegistry $registry, private readonly EntityManagerInterface $entityManager)
     {
-        parent::__construct($registry, News::class);
+        parent::__construct($registry, EntityNews::class);
     }
 
     public function persist(News $entity): void
