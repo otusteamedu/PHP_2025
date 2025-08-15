89d782056a5b9a5dbba3b93c8ff3bbc120290ae8
Марина Лубник <m.kalinina@21vek.by>
2025-08-15 09:53:41 +0300

    hm 10

--- app/Elasticsearch/ElasticIndex.php
+++ app/Elasticsearch/ElasticIndex.php
@@ -13,66 +13,6 @@ class ElasticIndex
         $this->client = $client;
     }
 
-    private function createBody(): array
-    {
-        return [
-            'mappings' => [
-                'properties' => [
-                    'title' => [
-                        'type' => 'text',
-                        'analyzer' => 'my_russian'
-                    ],
-                    'sku' => [
-                        'type' => 'keyword',
-                    ],
-                    'category' => [
-                        'type' => 'keyword',
-                        'analyzer' => 'my_russian'
-                    ],
-                    'price' => [
-                        'type' => 'integer',
-                    ],
-                    'stock' => [
-                        'type' => 'nested',
-                        'properties' => [
-                            'shop' => [
-                                'type' => 'keyword',
-                                'analyzer' => 'my_russian'
-                            ],
-                            'stock' => [
-                                'type' => 'integer'
-                            ],
-                        ]
-                    ]
-                ]
-            ],
-            'settings' => [
-                'analysis' => [
-                    'filter' => [
-                        'ru_stop' => [
-                            'type' => 'stop',
-                            'stopwords' => '_russian_'
-                        ],
-                        'ru_stemmer' => [
-                            'type' => 'stemmer',
-                            'language' => 'russian'
-                        ]
-                    ],
-                    'analyzer' => [
-                        'my_russian' => [
-                            'tokenizer' => 'standard',
-                            'filter' => [
-                                'lowercase',
-                                'ru_stop',
-                                'ru_stemmer'
-                            ]
-                        ]
-                    ]
-                ]
-            ],
-        ];
-    }
-
     public function createIndex(): void
     {
         $indexName = new ElasticInfo();
@@ -82,7 +22,7 @@ class ElasticIndex
 
             $params = [
                 'index' => $indexName->getIndexName(),
-                'body' => $this->createBody(),
+                'body' => $indexName->getMappingBody(),
             ];
 
             $response = $client->indices()->create($params);
--- app/Elasticsearch/ElasticInfo.php
+++ app/Elasticsearch/ElasticInfo.php
@@ -12,4 +12,65 @@ class ElasticInfo
     {
         return self::INDEX_NAME;
     }
+
+    public function getMappingBody(): array
+    {
+        return [
+            'mappings' => [
+                'properties' => [
+                    'title' => [
+                        'type' => 'text',
+                        'analyzer' => 'my_russian'
+                    ],
+                    'sku' => [
+                        'type' => 'keyword',
+                    ],
+                    'category' => [
+                        'type' => 'keyword',
+                        'analyzer' => 'my_russian'
+                    ],
+                    'price' => [
+                        'type' => 'integer',
+                    ],
+                    'stock' => [
+                        'type' => 'nested',
+                        'properties' => [
+                            'shop' => [
+                                'type' => 'keyword',
+                                'analyzer' => 'my_russian'
+                            ],
+                            'stock' => [
+                                'type' => 'integer'
+                            ],
+                        ]
+                    ]
+                ]
+            ],
+            'settings' => [
+                'analysis' => [
+                    'filter' => [
+                        'ru_stop' => [
+                            'type' => 'stop',
+                            'stopwords' => '_russian_'
+                        ],
+                        'ru_stemmer' => [
+                            'type' => 'stemmer',
+                            'language' => 'russian'
+                        ]
+                    ],
+                    'analyzer' => [
+                        'my_russian' => [
+                            'tokenizer' => 'standard',
+                            'filter' => [
+                                'lowercase',
+                                'ru_stop',
+                                'ru_stemmer'
+                            ]
+                        ]
+                    ]
+                ]
+            ],
+        ];
+
+    }
 }
