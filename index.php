2b132daf6fe724c42fcabfab4991e5df7d590cb6
AlexSamojlo <121355372+AlexSamojlo@users.noreply.github.com>
2025-08-22 16:14:35 +0300

    Update index.php

--- index.php
+++ index.php
@@ -3,13 +3,18 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 use App\User;
 
-$users = User::all();
-
-foreach ($users as $user) {
-    echo $user->getName() . " - " . $user->getEmail() . "\n";
-}
+$offset = 0;
+$limit = 100;
+do {
+    $users = User::all($limit, $offset);
+    foreach ($users as $user) {
+        echo $user->getName() . " - " . $user->getEmail() . "\n";
+    }
+    $offset += $limit;
+} while (count($users) === $limit);
 
 $user = User::find(1);
 if ($user) {
     echo $user->getName() . " - " . $user->getEmail();
 }
+
