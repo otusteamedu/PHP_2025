b4f3d9d81862d21a4502fe5cc079a47f49c87439
Ilya Gainutdinov <helibry@mail.ru>
2025-08-30 15:27:03 +0500

    hw2: Calculation using bc or awk

--- sum.sh
+++ sum.sh
@@ -6,16 +6,29 @@ if [ "$#" -ne 2 ]; then
 fi
 
 SUM=0;
+USE_BC=0;
+if command -v bc >/dev/null 2>&1; then
+  echo "Calculating using bc";
+  USE_BC=1;
+elif command -v awk >/dev/null 2>&1; then
+  echo "Calculating using awk";
+else
+  echo "awk or bc required to run this script";
+  exit 3;
+fi
 
 for number in "$@" ; do
-  echo "$number";
-  if [[ "$number" =~ ^-?[0-9]*$ ]]; then
-      SUM=$(("$SUM" + "$number"));
+  if [[ "$number" =~ ^[+-]?[0-9]+([.][0-9]+)?$ ]]; then
+    if [[ $USE_BC -eq 1 ]]; then
+      SUM=$(echo "$SUM + $number" | bc -l);
+    else
+      SUM=$(awk "BEGIN { print $SUM + $number }");
+    fi
   else
       echo "The input '$number' not number";
       exit 2;
   fi
 done
 
-echo "Sum is $SUM";
+echo "$SUM" | xargs printf "SUM is %.2f";
 exit 0;
