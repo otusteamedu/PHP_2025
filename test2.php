ca01bc80316c842c9d5b7e8658c2fcf682fd61a0
Марина Лубник <m.kalinina@21vek.by>
2025-09-06 16:07:45 +0300

    hm 18

---
+++ test2.php
@@ -0,0 +1,25 @@
+<?php
+
+class Solution
+{
+
+    /**
+     * @param Integer $numerator
+     * @param Integer $denominator
+     * @return String
+     */
+    function fractionToDecimal($numerator, $denominator)
+    {
+        $number = $numerator / $denominator;
+        $precision = 3;
+        $pos = strrpos($number, '.');
+        if ($pos !== false) {
+            $number = substr($number, 0, $pos + 1 + $precision);
+        }
+
+        return $number;
+    }
+}
+
+$result = new Solution()->fractionToDecimal(2, 1);
+var_dump($result);
