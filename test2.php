80d396653fa0eb3c9257d09823b098d85ac56894
Марина Лубник <m.kalinina@21vek.by>
2025-10-07 21:36:34 +0300

    hm 13

--- test2.php
+++ test2.php
@@ -1,12 +1,27 @@
 <?php
 
-class Solution {
+class Solution
+{
 
     /**
      * @param Integer[] $nums
      * @return Integer[]
      */
-    function frequencySort($nums) {
+    function frequencySort(array $nums): array
+    {
+        $freq = array_count_values($nums);
+
+        usort($nums, function($a, $b) use ($freq) {
+            if ($freq[$a] === $freq[$b]) {
+                return $b - $a;
+            }
+            return $freq[$a] - $freq[$b];
+        });
+
+        return $nums;
 
     }
 }
+
+$solution = new Solution();
+var_dump($solution->frequencySort([4, 4, 4, 1, 1, 2]));
