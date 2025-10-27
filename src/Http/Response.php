e7894f86fc50da84c95189a3da34c1242bd23cc9
Larkinov <buyantolarkinov@gmail.com>
2025-10-27 17:56:32 +0700

    fix: изменение типа для возвращаемого значения

--- src/Http/Response.php
+++ src/Http/Response.php
@@ -7,11 +7,7 @@ class Response
     public function send(int $httpcode, array|object|null $data)
     {
         header('Content-type: application/json');
-        if($data) echo json_encode($data);
+        if ($data) echo json_encode($data);
         http_response_code($httpcode);
-        if($httpcode===200)
-            exit(0);
-        else
-            exit(1);
     }
 }
