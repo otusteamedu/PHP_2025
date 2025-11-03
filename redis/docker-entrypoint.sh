c58c1849718154562f7390bb8f4a598b3e88a323
buglova <buglova@ujin.tech>
2025-11-04 01:11:45 +0300

    правка redis контейнера

--- redis/docker-entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -e
-
-# Заменяем переменные в redis.conf
-envsubst < /usr/local/etc/redis/redis.conf.template > /usr/local/etc/redis/redis.conf
-
-# Запускаем Redis
-exec redis-server /usr/local/etc/redis/redis.conf
