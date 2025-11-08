37229c114b46c61534155b21b27e8a35c2499052
AK <hooploop14@gmail.com>
2025-11-08 20:02:14 +0300

    sum of two numbers

---
+++ code/calculate.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+printf "Введите значение первого числа: "
+read -r a
+
+if ! echo "$a" | grep -qE '^-?[0-9]+(\.[0-9]+)?$'; then
+  echo "Ошибка: 'a' не является числом."
+  exit 1
+fi
+
+printf "Введите значение второго числа: "
+read -r b
+
+if ! echo "$b" | grep -qE '^-?[0-9]+(\.[0-9]+)?$'; then
+  echo "Ошибка: 'b' не является числом."
+  exit 1
+fi
+
+result=$(echo "$a + $b" | bc)
+echo "Сумма чисел $a и $b равна: $result"
