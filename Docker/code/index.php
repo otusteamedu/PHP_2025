e743f575b75f4788b50600aed360cac0e862a26e
ull123 <47916721+ull123@users.noreply.github.com>
2025-11-09 19:25:05 +0300

    redis and memc
    не удается проверить redis и мемкеш

--- Docker/code/index.php
+++ Docker/code/index.php
@@ -1,4 +1,13 @@
-<?php
-echo "Hello, Otus!";
-echo "Tratata Uraaaaa";
+<?
+declare(strict_types=1);
+
+$obMysqli = new mysqli('mysql', 'adminushka', 'mysql123', 'db123', 3306);
+if ($obMysqli->connect_error) {
+    die('Error connecting to MySQL: ' . $obMysqli->connect_error);
+}
+echo 'Connection to MySQL server established!';
+
+$obMysqli->close();
+
+echo "<br>Hello, Otus!";
 phpinfo();
