317bf593bd254ebdc60c677220c6cf9b181309f4
DaruHashida <f.khvostunkov@andagar.ru>
2025-11-11 12:20:15 +0300

    DZ-2: написаны башевские скрипты сумматора и сортировщика

---
+++ summator.sh
@@ -0,0 +1,20 @@
+#!/bin/env bash
+num1=$1
+num2=$2
+
+is_valid_number() {
+    [[ $1 =~ ^-?(0|[1-9][0-9]*)(\.[0-9]+)?$ ]]
+}
+
+if ! is_valid_number "$num1"; then
+    echo "Ошибка: '$num1' — некорректное число."
+    exit 0
+fi
+
+if ! is_valid_number "$num2"; then
+    echo "Ошибка: '$num2' — некорректное число."
+    exit 0
+fi
+
+sum=$(echo "$num1 + $num2" | bc)
+echo "$sum"
