9e40d3b55830c10d7c330555dc474c3b5e483478
buglova <buglova@ujin.tech>
2025-11-12 23:12:24 +0300

    Сложение отрицательных вещественных чисел без bc

--- bash/sum.sh
+++ bash/sum.sh
@@ -1,10 +1,49 @@
 #! /bin/bash
-if ! dpkg -s bcyy &>/dev/null; then
- apt install bc
- echo "Packet installed"
-fi
+if [[ $1 =~ ^-?[[:digit:]]+\.?[[:digit:]]*$ ]] && [[ $2 =~ ^-?[[:digit:]]+\.?[[:digit:]]*$ ]]; then 
+  # Разделяем числа на целую и дробную части
+  int1=${1%.*};
+  int2=${2%.*};
+  if [[ $1 == *.* ]]; then
+    frac1=${1#*.}
+  else
+    frac1=0
+  fi
 
-if [[ $1 =~ ^-?[[:digit:]]+\.?[[:digit:]]$ ]] && [[ $2 =~ ^-?[[:digit:]]+\.?[[:digit:]]*$ ]]; 
-then echo "scale=3;$1+$2" | bc
-else echo "Invalid parameters!"
+  if [[ $2 == *.* ]]; then
+    frac2=${2#*.}
+  else
+    frac2=0
+  fi
+
+  digits_count_frac1=${#frac1}
+  digits_count_frac2=${#frac2}
+
+  # Если целая часть отрицательная, делаем дробную часть тоже отрицательной
+  if [[ $int1 == -* ]]; then
+    frac1="-$frac1"
+  fi
+  if [[ $int2 == -* ]]; then
+    frac2="-$frac2"
+  fi
+
+  # Вычисляем максимальный порядок дробной части                
+  if [[ $digits_count_frac1 == $digits_count_frac2 ]]; then
+    multiplier=$digits_count_frac1
+    number1=$(($int1 * 10**$multiplier + $frac1))
+    number2=$(($int2 * 10**$multiplier + $frac2))
+  else
+    if [[ $digits_count_frac1 -gt $digits_count_frac2 ]]; then
+      multiplier=$digits_count_frac1
+      number1=$(($int1 * 10**$multiplier + $frac1))
+      number2=$(($int2 * 10**$multiplier + $frac2 * 10**($multiplier - $digits_count_frac2)))
+    else
+      multiplier=$digits_count_frac2
+      number1=$(($int1 * 10**$multiplier + $frac1 * 10**($multiplier - $digits_count_frac1)))
+      number2=$(($int2 * 10**$multiplier + $frac2))
+    fi
+  fi
+
+  printf %.3f\\n "$(($number1 + $number2))e-$multiplier"
+else 
+  echo "Invalid parameters!"
 fi
