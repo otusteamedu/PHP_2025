70bb42efa0488496c8b95d3a3a43a230c23200f6
vova <v.silkin@21vek.by>
2025-11-15 19:22:31 +0300

    HW3-Composer

--- hw-3/public/index.php
+++ hw-3/public/index.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 require __DIR__ . '/vendor/autoload.php';
 
-$WordCounter = new Ubunvova\WordCount\WordCounter();
-$checker = new App\CheckLibrary($WordCounter);
+$wordCounter = new Ubunvova\WordCount\WordCounter();
+$checker = new App\CheckLibrary($wordCounter);
 
 $checker->check();
