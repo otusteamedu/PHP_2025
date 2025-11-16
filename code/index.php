5ca11150206133e0ebae2de616c2e1bc05c81f21
AK <hooploop14@gmail.com>
2025-11-16 20:27:49 +0300

    подключение пакета Ak14\Exchange.
    Вывод списка валют

--- code/index.php
+++ code/index.php
@@ -1,2 +1,19 @@
 <?php
 require __DIR__ . '/vendor/autoload.php';
+
+use Ak14\Exchange\Exchange;
+
+$nbrb = new Exchange();
+
+if($currencies = new Exchange()->currencies())
+{
+    echo "<table>";
+    echo "<tr><th>Abbreviation</th><th>Name</th></tr>";
+    foreach ($currencies as $currency) {
+        echo "<tr>";
+        echo "<td>".$currency['Cur_Abbreviation']."</td>";
+        echo "<td>".$currency['Cur_Name']."</td>";
+        echo "</tr>";
+    }
+    echo "</table>";
+};
