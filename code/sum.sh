904062b2b33db7b19243d7794caef8549f6fd3bf
Maksim Skoropiscev <skoro11@bk.ru>
2025-11-17 17:04:10 +0300

    feat: Added checking operands for a number

--- code/sum.sh
+++ code/sum.sh
@@ -6,3 +6,22 @@ then
   exit 1
 fi
 
+OPERAND_1=$1
+OPERAND_2=$2
+
+function isNumber() {
+  re='^-?[0-9]+([.][0-9]+)?$'
+  [[ $1 =~ $re ]]
+}
+
+if ! isNumber "$OPERAND_1";
+then
+  echo "Первый операнд не является числом"
+  exit 1
+fi
+
+if ! isNumber "$OPERAND_2";
+then
+  echo "Второй операнд не является числом"
+  exit 1
+fi
