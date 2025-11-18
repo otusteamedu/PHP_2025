9b30ce964aea9a4d26d4a7e75c98732713f0eb3e
vova <v.silkin@21vek.by>
2025-11-18 22:08:03 +0300

    HW3-Composer

--- hw-3/public/index.php
+++ hw-3/public/index.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 
 $wordCounter = new Ubunvova\WordCount\WordCounter();
 $checker = new App\CheckLibrary($wordCounter);
