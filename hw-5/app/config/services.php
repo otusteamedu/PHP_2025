8390936828a8ec1ed2ca26696423a6d71ac2c60e
vova <v.silkin@21vek.by>
2025-11-23 12:14:17 +0300

    HW5-EmailVerification

---
+++ hw-5/app/config/services.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Application\VerificationEmailService;
+use App\Infrastructure\CheckedEmailsFileWriter;
+use App\Infrastructure\DnsEmailVerifier;
+use App\Infrastructure\EmailsFileReader;
+use App\UserInterface\VerificationEmailCommand;
+
+$emailsFilePath = dirname(__DIR__) . '/resources/emails.txt';
+$checkedEmailsFilePath = dirname(__DIR__) . '/resources/checked_emails.txt';
+
+return [
+    'dnsEmailVerifier' => function() {
+        return new DnsEmailVerifier();
+    },
+
+    'emailsFileReader' => function() use ($emailsFilePath) {
+        return new EmailsFileReader($emailsFilePath);
+    },
+
+    'checkedEmailsFileWriter' => function() use ($checkedEmailsFilePath) {
+        return new CheckedEmailsFileWriter($checkedEmailsFilePath);
+    },
+
+    'verificationEmailService' => function($container) {
+        return new VerificationEmailService(
+            $container->get('checkedEmailsFileWriter'),
+            $container->get('emailsFileReader'),
+            $container->get('dnsEmailVerifier'),
+        );
+    },
+
+    'verificationEmailCommand' => function($container) {
+        return new VerificationEmailCommand(
+            $container->get('verificationEmailService'),
+        );
+    }
+];
--- hw-5/app/public/index.php
+++ hw-5/app/public/index.php
@@ -4,13 +4,8 @@ declare(strict_types=1);
 
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 
-$emailsFilePath = dirname(__DIR__) . '/resources/emails.txt';
-$checkedEmailsFilePath = dirname(__DIR__) . '/resources/checked_emails.txt';
+$definitions = require dirname(__DIR__) . '/config/services.php';
+$container = new App\Infrastructure\DI\Container($definitions);
 
-$dnsEmailVerifier = new App\Infrastructure\DnsEmailVerifier();
-$emailsFileReader = new App\Infrastructure\EmailsFileReader($emailsFilePath);
-$checkedEmailsFileWriter = new App\Infrastructure\CheckedEmailsFileWriter($checkedEmailsFilePath);
-$verificationEmailService = new App\Application\VerificationEmailService($checkedEmailsFileWriter, $emailsFileReader, $dnsEmailVerifier);
-$verificationEmailCommand = new App\UserInterface\VerificationEmailCommand($verificationEmailService);
-
+$verificationEmailCommand = $container->get('verificationEmailCommand');
 $verificationEmailCommand->handle();
