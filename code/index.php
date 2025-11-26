759b6e8c8f1ca57cf57c958ce96b0b97644f8f96
Maksim Skoropiscev <skoro11@bk.ru>
2025-11-26 17:05:36 +0300

    feat: Added index page

---
+++ code/index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="ru">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Hello, Otus!</title>
+</head>
+<body>
+<div class="container">
+	<h3 class="container__title">Запрос обработал контейнер:
+		<span>container_id=<?php echo $_SERVER['HOSTNAME'];?></span>
+	</h3>
+</div>
+</body>
+</html>
