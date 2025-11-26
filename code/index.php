25ed928136008805a96bdcbb32251410f629a71b
Maksim Skoropiscev <skoro11@bk.ru>
2025-11-26 18:48:41 +0300

    feat: Added counter session

--- code/index.php
+++ code/index.php
@@ -1,3 +1,15 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['counter'])) {
+    $_SESSION['counter'] = 1;
+} else {
+    $_SESSION['counter']++;
+}
+
+$sessionId = session_id();
+$container = $_SERVER['HOSTNAME'];
+?>
 <!doctype html>
 <html lang="ru">
 <head>
@@ -8,12 +20,14 @@
 <body>
 <div class="container">
 	<h3 class="container__title">Запрос обработал контейнер:
-		<span>container_id=<?php echo $_SERVER['HOSTNAME'];?></span>
+		<span>container_id=<?= $container ?></span>
 	</h3>
 	<?php require_once $_SERVER['DOCUMENT_ROOT'] . '/redis.php';  ?>
 	<?php if (isset($containerIdFromRedis)):?>
 		<p>redis container_id=<?= $containerIdFromRedis ?></p>
 	<?php endif;?>
+	<p>Session ID: <?= $sessionId ?></p>
+	<p>Session Counter: <?= $_SESSION['counter'] ?></p>
 </div>
 </body>
 </html>
