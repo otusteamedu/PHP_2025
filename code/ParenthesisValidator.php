e7a2a59e0d9a8d5939f8b53188a5466e3695f387
Maksim Skoropiscev <skoro11@bk.ru>
2025-11-27 10:00:17 +0300

    feat: Checking for correct nesting of parentheses

--- code/ParenthesisValidator.php
+++ code/ParenthesisValidator.php
@@ -3,10 +3,13 @@
 class ParenthesisValidator
 {
     const ACCEPTABLE_CHARACTERS = ['(', ')'];
+    const PAIRED_PARENTHESIS = '()';
+    private string $parenthesis;
     private array $characters;
 
     public function __construct(string $str)
     {
+        $this->parenthesis = $str;
         $this->characters = str_split($str);
     }
 
@@ -16,6 +19,14 @@ class ParenthesisValidator
     public function validate(): void
     {
         $this->checkAcceptableCharacters();
+        $copyParenthesis = $this->parenthesis;
+        while ($this->isStringContainsPairedParenthesis($copyParenthesis)) {
+            $this->removePairedParenthesis($copyParenthesis);
+        }
+
+        if($copyParenthesis != '') {
+            throw new Exception('Parenthesis are placed incorrectly');
+        }
     }
 
     /**
@@ -27,5 +38,15 @@ class ParenthesisValidator
             throw new Exception('An empty string was passed');
         if(!array_diff($this->characters, self::ACCEPTABLE_CHARACTERS) == [])
             throw new Exception('String must not contain characters other than "(" and ")"');
+    }
+
+    private function removePairedParenthesis(string &$str): void
+    {
+        $str = str_replace(self::PAIRED_PARENTHESIS, '', $str);
+    }
+
+    private function isStringContainsPairedParenthesis(string $str): bool
+    {
+        return str_contains($str, self::PAIRED_PARENTHESIS);
     }
 }
