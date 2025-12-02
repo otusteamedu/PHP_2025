0ece287a1ab71f85e3403b9be3b9a342f75abfbd
Maksim Skoropiscev <skoro11@bk.ru>
2025-12-02 07:57:21 +0300

    Revert "feat: Added the installation of bc package"
    This reverts commit 3881d5c2aa7e58a86138622f90c56c36ea8ca23f.

--- code/sum.sh
+++ code/sum.sh
@@ -26,28 +26,4 @@ then
   exit 1
 fi
 
-function installBc() {
- if ! command -v bc >/dev/null 2>&1; then
-   echo "bc не найден, устанавливаю..."
-   if [ -f /etc/alpine-release ]; then
-       echo "Обнаружен Alpine Linux"
-       apk add --no-cache bc || { echo "Ошибка установки bc"; exit 1; }
-
-   elif [ -f /etc/debian_version ]; then
-       echo "Обнаружен Debian/Ubuntu"
-       apt-get update && apt-get install -y bc || { echo "Ошибка установки bc"; exit 1; }
-
-   elif [ -f /etc/arch-release ]; then
-       echo "Обнаружен Arch Linux"
-       pacman -Sy --noconfirm bc || { echo "Ошибка установки bc"; exit 1; }
-
-   else
-       echo "Неизвестный дистрибутив — не могу установить dc автоматически"
-       exit 1
-   fi
- fi
-}
-
-installBc
-
 echo "$1 + $2" | bc
