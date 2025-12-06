1077b27a41487c502547ac5de930a6a8e4c44150
arlex2305k <alex@digsol.ru>
2025-12-06 20:29:23 +0300

    HW#7

--- test.php
+++ test.php
@@ -1,40 +1,27 @@
 <?php
 require __DIR__ . '/vendor/autoload.php';
 
-use Arlex2305k\EmailsVerifier\Service;
+use Arlex2305k\MergeLists\{ListHelper, Solution};
 
-$emails = [
-	// Валидные
-	'user@example.com',
-	'test.email@domain.org',
-	'user+tag@example.net',
-	'firstname.lastname@company.co.uk',
-	'user123@test-domain.com',
-	// Невалидные
-	'invalid-email',
-	'@invalid.com',
-	'user@',
-	'user..double.dot@example.com',
-	'user@.com',
-	'.user@domain.com',
-	// Невалидные - MX
-	'user@nonexistentdomain12345.com',
-	'test@foolishdomain999.ru',
-	'user@sub.domain.example.ru',
-	'user_name@example.info',
-];
+$objSolution = new Solution();
 
-$service = new Service($emails);
-$result = $service->verify();
+// Test1
+$list1 = ListHelper::createFromArray([1,2,4]);
+$list2 = ListHelper::createFromArray([1,3,4]);
+echo "--- Test1 ---\nList1: $list1\nList2: $list2\n";
+$totalList = $objSolution->mergeTwoLists($list1, $list2);
+echo "Total: $totalList\n\n";
 
-echo "Проверка Email [Всего: {$service->getEmailsCount()} | Валидны: {$service->getValidCount()} | Невалидны: {$service->getInvalidCount()}]:\n";
-echo "==========================\n";
+// Test2
+$list1 = ListHelper::createFromArray([]);
+$list2 = ListHelper::createFromArray([]);
+echo "--- Test2 ---\nList1: $list1\nList2: $list2\n";
+$totalList = $objSolution->mergeTwoLists($list1, $list2);
+echo "Total: $totalList\n\n";
 
-foreach ($service->getArEmails() as $email) {
-	if (!$email->hasErrors()) {
-		echo "$email : ok.\n";
-	} else {
-		$strErrors = implode(' | ', $email->getErrors());
-		echo "$email : $strErrors\n";
-	}
-}
+// Test3
+$list1 = ListHelper::createFromArray([]);
+$list2 = ListHelper::createFromArray([0]);
+echo "--- Test3 ---\nList1: $list1\nList2: $list2\n";
+$totalList = $objSolution->mergeTwoLists($list1, $list2);
+echo "Total: $totalList\n\n";
