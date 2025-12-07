72bf4c600de4b1de78744238f5323b6f48b9ed86
Maksim Skoropiscev <skoro11@bk.ru>
2025-12-07 18:46:52 +0300

    feat: Init decision

---
+++ code/decision.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+class ListNode {
+    public $val = 0;
+    public $next = null;
+    function __construct($val = 0, $next = null) {
+        $this->val = $val;
+        $this->next = $next;
+    }
+}
+class Solution {
+    function mergeTwoLists(ListNode|null $list1, ListNode|null $list2): ListNode|null
+    {
+        return $list1;
+    }
+}
