ddbc400b23a71699b08816f72ff28d8c0980f6bf
Maksim Skoropiscev <skoro11@bk.ru>
2025-12-07 19:03:31 +0300

    feat: Checking nodes for null

--- code/decision.php
+++ code/decision.php
@@ -13,6 +13,9 @@ class ListNode {
 class Solution {
     function mergeTwoLists(ListNode|null $list1, ListNode|null $list2): ListNode|null
     {
+        if (is_null($list1)) return $list2;
+        if (is_null($list2)) return $list1;
+
         return $list1;
     }
 }
