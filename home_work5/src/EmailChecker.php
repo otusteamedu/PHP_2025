b241081343649cbbcde2377e82cd1653f897cc8a
Igor Morozov <GreekIgor87@gmail.com>
2025-12-11 15:00:39 +0500

    задание номер 5

---
+++ home_work5/src/EmailChecker.php
@@ -0,0 +1,20 @@
+<?php
+namespace App;
+class EmailChecker
+{
+    private const EMAIL_REGEX = '/^([a-zA-Z0-9._%+-]+)@([a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/';
+    
+    public function checkSyntax($email)
+    {
+        if (preg_match(self::EMAIL_REGEX, $email, $matches)) {
+            return $matches[2];
+        }
+        return false;
+    }
+    
+    public function checkDns($domain)
+    {
+        $mx = dns_get_record($domain, DNS_MX);
+        return !empty($mx);
+    }
+}
