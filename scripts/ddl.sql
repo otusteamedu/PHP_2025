5eb1bed99392c7308ec65ce99b62cca428384e39
Alexander <system.bin@yandex.ru>
2025-12-14 16:04:23 +0500

    hw-8

--- scripts/ddl.sql
+++ scripts/ddl.sql
@@ -86,3 +86,129 @@ create unique index tickets_seance_id_hall_seat_id_uindex
 
 create index tickets_seance_id_index
     on public.tickets (seance_id);
+
+create table public.movie_entity_attribute_types
+(
+    id   bigserial
+        constraint movie_entity_attribute_types_pk
+            primary key,
+    type varchar not null
+        constraint movie_entity_attribute_types_type_check
+            check (
+                (type)::text = ANY
+                ((ARRAY [
+                    'string'::character varying,
+                    'text'::character varying,
+                    'integer'::character varying,
+                    'double'::character varying,
+                    'boolean'::character varying,
+                    'date'::character varying,
+                    'time'::character varying,
+                    'datetime'::character varying
+                    ])::text[])
+                )
+);
+
+create unique index movie_entity_attribute_types_type_uindex
+    on public.movie_entity_attribute_types (type);
+
+create table public.movie_entity_attributes
+(
+    id                             bigserial
+        constraint movie_entity_attributes_pk
+            primary key,
+    movie_entity_attribute_type_id bigint   not null
+        constraint movie_entity_attributes_movie_entity_attribute_types_id_fk
+            references public.movie_entity_attribute_types
+            on update restrict on delete cascade,
+    attribute                      varchar  not null,
+    mode                           smallint not null
+);
+
+create index movie_entity_attributes_movie_entity_attribute_type_id_index
+    on public.movie_entity_attributes (movie_entity_attribute_type_id);
+
+create index movie_entity_attributes_mode_index
+    on public.movie_entity_attributes (mode);
+
+create table public.movie_entity_values
+(
+    id                        bigserial
+        constraint movie_entity_values_pk
+            primary key,
+    movie_entity_attribute_id bigint not null
+        constraint movie_entity_values_movie_entity_attributes_id_fk
+            references public.movie_entity_attributes
+            on update restrict on delete cascade,
+    movie_id                  bigint not null
+        constraint movie_entity_values_movies_id_fk
+            references public.movies
+            on update restrict on delete cascade,
+    value_string              varchar,
+    value_text                text,
+    value_integer             bigint,
+    value_double              double precision,
+    value_boolean             boolean,
+    value_date                date,
+    value_time                time,
+    value_datetime            timestamp,
+    constraint movie_entity_values_exactly_one_value_check
+        check ((((((((((value_string IS NOT NULL))::integer + ((value_text IS NOT NULL))::integer) +
+                     ((value_integer IS NOT NULL))::integer) + ((value_double IS NOT NULL))::integer) +
+                   ((value_boolean IS NOT NULL))::integer) + ((value_date IS NOT NULL))::integer) +
+                 ((value_time IS NOT NULL))::integer) + ((value_datetime IS NOT NULL))::integer) = 1)
+);
+
+create index movie_entity_values_movie_entity_attribute_id_index
+    on public.movie_entity_values (movie_entity_attribute_id);
+
+create index movie_entity_values_movie_id_index
+    on public.movie_entity_values (movie_id);
+
+create unique index movie_entity_values_movie_entity_attribute_id_movie_id_uindex
+    on public.movie_entity_values (movie_entity_attribute_id, movie_id);
+
+create view public.movie_attributes(title, type, attribute, mode, value) as
+SELECT m.title,
+       meat.type,
+       mea.attribute,
+       mea.mode,
+       COALESCE(
+           mev.value_string,
+           mev.value_text::character varying,
+           mev.value_integer::text::character varying,
+           mev.value_double::text::character varying,
+           mev.value_boolean::text::character varying,
+           mev.value_date::text::character varying,
+           mev.value_time::text::character varying,
+           mev.value_datetime::text::character varying
+       ) AS value
+FROM movies m
+         JOIN movie_entity_values mev ON m.id = mev.movie_id
+         JOIN movie_entity_attributes mea ON mea.id = mev.movie_entity_attribute_id
+         JOIN movie_entity_attribute_types meat ON meat.id = mea.movie_entity_attribute_type_id
+ORDER BY m.id, mea.id;
+
+create view public.movie_attributes_public(title, type, attribute, value) as
+SELECT title,
+       type,
+       attribute,
+       value
+FROM movie_attributes
+WHERE (mode::integer & 1) <> 0;
+
+create view public.movie_attributes_service(title, type, attribute, value) as
+SELECT title,
+       type,
+       attribute,
+       value
+FROM movie_attributes
+WHERE (mode::integer & 2) <> 0;
+
+create view public.movie_attributes_marketing(title, type, attribute, value) as
+SELECT title,
+       type,
+       attribute,
+       value
+FROM movie_attributes
+WHERE (mode::integer & 4) <> 0;
--- scripts/dml.sql
+++ scripts/dml.sql
@@ -3,13 +3,13 @@ values (1, 'A'),
        (2, 'B'),
        (3, 'C');
 
-insert into public.hall_seats (id, hall_id, row, col, number, seat_type)
-values (1, 1, 1, 1, 'A1', 'seat'),
-       (2, 1, 1, 2, 'A2', 'seat'),
-       (3, 1, 1, 3, 'A3', 'seat'),
-       (4, 2, 1, 1, 'B1', 'seat'),
-       (5, 2, 1, 2, 'B2', 'seat'),
-       (6, 3, 1, 1, 'C1', 'seat');
+insert into public.hall_seats (id, hall_id, number, row, col, seat_type)
+values (1, 1, 'A1', 1, 1, 'seat'),
+       (2, 1, 'A2', 1, 2, 'seat'),
+       (3, 1, 'A3', 1, 3, 'seat'),
+       (4, 2, 'B1', 1, 1, 'seat'),
+       (5, 2, 'B2', 1, 2, 'seat'),
+       (6, 3, 'C1', 1, 1, 'seat');
 
 insert into public.movies (id, title)
 values (1, 'A'),
@@ -28,12 +28,54 @@ values (1, '2025-11-25 12:00:00.000000', '2025-11-25 13:00:00.000000', 1, 1, 100
        (7, '2025-11-25 21:00:00.000000', '2025-11-25 22:00:00.000000', 1, 5, 300),
        (8, '2025-11-25 22:00:00.000000', '2025-11-25 23:00:00.000000', 3, 5, 300);
 
-insert into public.tickets (id, price, hall_seat_id, seance_id)
+insert into public.tickets (id, price, seance_id, hall_seat_id)
 values (1, 100, 1, 1),
-       (2, 200, 2, 4),
-       (3, 300, 3, 7),
-       (4, 100, 4, 2),
+       (2, 200, 4, 2),
+       (3, 300, 7, 3),
+       (4, 100, 2, 4),
        (5, 200, 5, 5),
-       (6, 100, 6, 3),
+       (6, 100, 3, 6),
        (7, 200, 6, 6),
-       (8, 300, 6, 8);
+       (8, 300, 8, 6);
+
+insert into public.movie_entity_attribute_types (id, type)
+values (1, 'string'),
+       (2, 'text'),
+       (3, 'integer'),
+       (4, 'double'),
+       (5, 'boolean'),
+       (6, 'date'),
+       (7, 'time'),
+       (8, 'datetime');
+
+insert into public.movie_entity_attributes (id, movie_entity_attribute_type_id, attribute, mode)
+values (1, 2, 'Рецензии', 1),
+       (2, 5, 'Премия', 1),
+       (3, 6, 'Мировая премьера', 1),
+       (4, 6, 'Премьера в РФ', 1),
+       (5, 8, 'Дата начала продажи билетов', 7),
+       (6, 8, 'Дата запуска рекламы', 6),
+       (7, 4, 'Рейтинг', 5);
+
+insert into public.movie_entity_values (id, movie_entity_attribute_id, movie_id, value_string, value_text,
+                                        value_integer, value_double, value_boolean, value_date, value_time,
+                                        value_datetime)
+values (1, 1, 1, null, 'Отличный фильм, сильная режиссура', null, null, null, null, null, null),
+       (2, 1, 2, null, 'Затянуто, но красивая картинка', null, null, null, null, null, null),
+       (3, 1, 3, null, 'Лучший фильм года', null, null, null, null, null, null),
+       (4, 2, 1, null, null, null, null, true, null, null, null),
+       (5, 2, 3, null, null, null, null, true, null, null, null),
+       (6, 2, 4, null, null, null, null, false, null, null, null),
+       (7, 3, 1, null, null, null, null, null, '2026-01-12', null, null),
+       (8, 3, 2, null, null, null, null, null, '2026-01-01', null, null),
+       (9, 3, 3, null, null, null, null, null, '2026-01-20', null, null),
+       (10, 4, 1, null, null, null, null, null, '2026-01-01', null, null),
+       (11, 4, 2, null, null, null, null, null, '2026-01-15', null, null),
+       (12, 4, 4, null, null, null, null, null, '2026-01-10', null, null),
+       (13, 5, 1, null, null, null, null, null, null, null, '2026-01-01 10:00:00.000000'),
+       (14, 5, 2, null, null, null, null, null, null, null, '2026-01-20 09:00:00.000000'),
+       (15, 5, 3, null, null, null, null, null, null, null, '2026-01-01 12:00:00.000000'),
+       (16, 6, 1, null, null, null, null, null, null, null, '2026-01-15 08:00:00.000000'),
+       (17, 6, 2, null, null, null, null, null, null, null, '2026-01-01 08:30:00.000000'),
+       (18, 6, 5, null, null, null, null, null, null, null, '2026-01-10 09:00:00.000000'),
+       (19, 7, 1, null, null, null, 5.7, null, null, null, null);
