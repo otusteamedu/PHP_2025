b2483d6386aafd18cfaaf6e01139db55c6585a7f
AK <hooploop14@gmail.com>
2025-12-17 19:38:18 +0300

    Перенес создание таблиц customers halls sessions tickets

--- code/dump.sql
+++ code/dump.sql
@@ -26,7 +26,7 @@ SET default_table_access_method = heap;
 -- Name: attributes; Type: TABLE; Schema: public; Owner: AK
 --
 
-CREATE TABLE public.attributes (
+CREATE TABLE public.attributes(
     id integer NOT NULL,
     title character varying
 );
@@ -38,7 +38,7 @@ ALTER TABLE public.attributes OWNER TO "AK";
 -- Name: attributes_types; Type: TABLE; Schema: public; Owner: AK
 --
 
-CREATE TABLE public.attributes_types (
+CREATE TABLE public.attributes_types(
     id integer NOT NULL,
     title character varying
 );
@@ -50,126 +50,212 @@ ALTER TABLE public.attributes_types OWNER TO "AK";
 -- Name: films; Type: TABLE; Schema: public; Owner: AK
 --
 
-CREATE TABLE public.films (
-    title character varying,
-    id integer NOT NULL
+CREATE TABLE public.films(
+    id          bigint NOT NULL,
+    title       character varying,
+    description text
 );
 
+ALTER TABLE public.films
+    OWNER TO "AK";
 
-ALTER TABLE public.films OWNER TO "AK";
+CREATE TABLE public.customers
+(
+    id    SERIAL PRIMARY KEY,
+    name  TEXT,
+    phone TEXT
+);
+
+ALTER TABLE public.customers
+    OWNER TO "AK";
+
+CREATE TABLE public.halls
+(
+    id     SERIAL PRIMARY KEY,
+    places INTEGER,
+    rate   NUMERIC,
+    title  VARCHAR(255)
+);
+
+ALTER TABLE public.halls
+    OWNER TO "AK";
+
+CREATE TABLE public.sessions
+(
+    id      SERIAL PRIMARY KEY,
+    film_id BIGINT,
+    date    TIMESTAMP NOT NULL,
+    price   DECIMAL(10, 2),
+    CONSTRAINT sessions_films_id_fk
+        FOREIGN KEY (film_id) REFERENCES public.films (id) ON DELETE SET NULL
+);
+
+ALTER TABLE public.sessions
+    OWNER TO "AK";
+
+CREATE TABLE public.session_hall
+(
+    session_id INTEGER NOT NULL,
+    hall_id    INTEGER NOT NULL,
+    PRIMARY KEY (session_id, hall_id),
+    CONSTRAINT fk_session
+        FOREIGN KEY (session_id) REFERENCES public.sessions (id) ON DELETE CASCADE,
+    CONSTRAINT fk_hall
+        FOREIGN KEY (hall_id) REFERENCES public.halls (id) ON DELETE CASCADE
+);
+
+ALTER TABLE public.session_hall
+    OWNER TO "AK";
+
+CREATE TABLE public.tickets
+(
+    id         SERIAL PRIMARY KEY,
+    session_id INT NOT NULL,
+    seat       INT NOT NULL,
+    CONSTRAINT fk_tickets_session
+        FOREIGN KEY (session_id) REFERENCES public.sessions (id) ON DELETE CASCADE,
+    UNIQUE (session_id, seat)
+);
+
+ALTER TABLE public.tickets
+    OWNER TO "AK";
+
+
+CREATE TABLE public.customer_tickets
+(
+    customer_id INTEGER NOT NULL,
+    ticket_id   INTEGER NOT NULL,
+    PRIMARY KEY (customer_id, ticket_id),
+    CONSTRAINT uq_ticket_id UNIQUE (ticket_id),
+    CONSTRAINT fk_customer
+        FOREIGN KEY (customer_id) REFERENCES public.customers (id) ON DELETE CASCADE,
+    CONSTRAINT fk_tickets
+        FOREIGN KEY (ticket_id) REFERENCES public.tickets (id) ON DELETE CASCADE
+);
+
+ALTER TABLE public.customer_tickets
+    OWNER TO "AK";
 
 --
 -- Name: values; Type: TABLE; Schema: public; Owner: AK
 --
 
-CREATE TABLE public."values" (
-    id integer NOT NULL,
-    type_id integer,
-    attribute_id integer,
-    film_id bigint,
+CREATE TABLE public."values"
+(
+    id            integer NOT NULL,
+    type_id       integer,
+    attribute_id  integer,
+    film_id       bigint,
     value_numeric float4,
-    value_date date,
+    value_date    date,
     value_boolean boolean,
-    value_text text
+    value_text    text
 );
 
 
-ALTER TABLE public."values" OWNER TO "AK";
+ALTER TABLE public."values"
+    OWNER TO "AK";
 
 --
 -- Name: about_films; Type: VIEW; Schema: public; Owner: AK
 --
 
 CREATE VIEW public.about_films AS
- SELECT f.title AS film_title,
-    at.title AS attribute_type_title,
-    a.title AS attribute_title,
-    COALESCE(v.value_text, v.value_numeric::text, v.value_date::text, v.value_boolean::text) AS value
-   FROM public."values" v
-     JOIN public.films f ON v.film_id = f.id
-     JOIN public.attributes_types at ON v.type_id = at.id
-     JOIN public.attributes a ON v.attribute_id = a.id
-  ORDER BY f.id;
+SELECT f.title                                                                                  AS film_title,
+       at.title                                                                                 AS attribute_type_title,
+       a.title                                                                                  AS attribute_title,
+       COALESCE(v.value_text, v.value_numeric::text, v.value_date::text, v.value_boolean::text) AS value
+FROM public."values" v
+         JOIN public.films f ON v.film_id = f.id
+         JOIN public.attributes_types at ON v.type_id = at.id
+         JOIN public.attributes a ON v.attribute_id = a.id
+ORDER BY f.id;
 
 
-ALTER TABLE public.about_films OWNER TO "AK";
+ALTER TABLE public.about_films
+    OWNER TO "AK";
 
 --
 -- Name: about_films_json; Type: VIEW; Schema: public; Owner: AK
 --
 
 CREATE VIEW public.about_films_json AS
- SELECT f.title AS film_title,
-    jsonb_object_agg(a.title,
-        CASE
-            WHEN v.value_numeric IS NOT NULL THEN to_jsonb(v.value_numeric)
-            WHEN v.value_date IS NOT NULL THEN to_jsonb(v.value_date)
-            WHEN v.value_boolean IS NOT NULL THEN to_jsonb(v.value_boolean)
-            WHEN v.value_text IS NOT NULL THEN to_jsonb(v.value_text)
-            ELSE 'null'::jsonb
-        END) AS attributes
-   FROM public."values" v
-     JOIN public.films f ON v.film_id = f.id
-     JOIN public.attributes a ON v.attribute_id = a.id
-  GROUP BY f.id, f.title;
+SELECT f.title                   AS film_title,
+       jsonb_object_agg(a.title,
+                        CASE
+                            WHEN v.value_numeric IS NOT NULL THEN to_jsonb(v.value_numeric)
+                            WHEN v.value_date IS NOT NULL THEN to_jsonb(v.value_date)
+                            WHEN v.value_boolean IS NOT NULL THEN to_jsonb(v.value_boolean)
+                            WHEN v.value_text IS NOT NULL THEN to_jsonb(v.value_text)
+                            ELSE 'null'::jsonb
+                            END) AS attributes
+FROM public."values" v
+         JOIN public.films f ON v.film_id = f.id
+         JOIN public.attributes a ON v.attribute_id = a.id
+GROUP BY f.id, f.title;
 
 
-ALTER TABLE public.about_films_json OWNER TO "AK";
+ALTER TABLE public.about_films_json
+    OWNER TO "AK";
 
 --
 -- Name: attributes_id_seq; Type: SEQUENCE; Schema: public; Owner: AK
 --
 
-ALTER TABLE public.attributes ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
-    SEQUENCE NAME public.attributes_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1
-);
+ALTER TABLE public.attributes
+    ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
+        SEQUENCE NAME public.attributes_id_seq
+        START WITH 1
+        INCREMENT BY 1
+        NO MINVALUE
+        NO MAXVALUE
+        CACHE 1
+        );
 
 
 --
 -- Name: attributes_types_id_seq; Type: SEQUENCE; Schema: public; Owner: AK
 --
 
-ALTER TABLE public.attributes_types ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
-    SEQUENCE NAME public.attributes_types_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1
-);
+ALTER TABLE public.attributes_types
+    ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
+        SEQUENCE NAME public.attributes_types_id_seq
+        START WITH 1
+        INCREMENT BY 1
+        NO MINVALUE
+        NO MAXVALUE
+        CACHE 1
+        );
 
 
 --
 -- Name: attributes_values_id_seq; Type: SEQUENCE; Schema: public; Owner: AK
 --
 
-ALTER TABLE public."values" ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
-    SEQUENCE NAME public.attributes_values_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1
-);
+ALTER TABLE public."values"
+    ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
+        SEQUENCE NAME public.attributes_values_id_seq
+        START WITH 1
+        INCREMENT BY 1
+        NO MINVALUE
+        NO MAXVALUE
+        CACHE 1
+        );
 
 
 --
 -- Name: films_id_seq; Type: SEQUENCE; Schema: public; Owner: AK
 --
 
-ALTER TABLE public.films ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
-    SEQUENCE NAME public.films_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1
-);
+ALTER TABLE public.films
+    ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
+        SEQUENCE NAME public.films_id_seq
+        START WITH 1
+        INCREMENT BY 1
+        NO MINVALUE
+        NO MAXVALUE
+        CACHE 1
+        );
 
 
 --
@@ -177,48 +263,49 @@ ALTER TABLE public.films ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
 --
 
 CREATE VIEW public.service_dates AS
- SELECT f.title,
-    string_agg(CASE WHEN v.value_date = CURRENT_DATE THEN a.title END, ', ') AS today,
-    string_agg(CASE WHEN v.value_date > CURRENT_DATE AND v.value_date <= (CURRENT_DATE + 20) THEN a.title END, ', ') AS period
-   FROM public."values" v
-     JOIN public.attributes a ON v.attribute_id = a.id
-     JOIN public.films f ON f.id = v.film_id
-  WHERE v.value_date IS NOT NULL
-  GROUP BY f.id, f.title
-  ORDER BY f.title;
+SELECT f.title,
+       string_agg(CASE WHEN v.value_date = CURRENT_DATE THEN a.title END, ', ') AS today,
+       string_agg(CASE WHEN v.value_date > CURRENT_DATE AND v.value_date <= (CURRENT_DATE + 20) THEN a.title END,
+                  ', ')                                                         AS period
+FROM public."values" v
+         JOIN public.attributes a ON v.attribute_id = a.id
+         JOIN public.films f ON f.id = v.film_id
+WHERE v.value_date IS NOT NULL
+GROUP BY f.id, f.title
+ORDER BY f.title;
 
 
-ALTER TABLE public.service_dates OWNER TO "AK";
+ALTER TABLE public.service_dates
+    OWNER TO "AK";
 
 --
 -- Name: workd; Type: VIEW; Schema: public; Owner: AK
 --
 
 CREATE VIEW public.workd AS
- WITH dates AS (
-         SELECT v.id,
-            v.value_date AS value,
-            a.title,
-            v.film_id
-           FROM public."values" v
-             JOIN public.attributes_types t ON v.type_id = t.id
-             LEFT JOIN public.attributes a ON a.id = v.attribute_id
-          WHERE t.title = 'date' AND v.id IN (
-              SELECT v2.id
-              FROM public."values" v2
-              JOIN public.attributes a_1 ON v2.attribute_id = a_1.id
-              WHERE a_1.title = ANY (ARRAY['начало продаж', 'начало рекламы'])
-          )
-    )
- SELECT f.title AS film,
-    d.title AS action,
-    d.value AS date
-   FROM dates d
-     JOIN public.films f ON f.id = d.film_id
-  WHERE d.value >= CURRENT_DATE AND d.value <= (CURRENT_DATE + 20);
+WITH dates AS (SELECT v.id,
+                      v.value_date AS value,
+                      a.title,
+                      v.film_id
+               FROM public."values" v
+                        JOIN public.attributes_types t ON v.type_id = t.id
+                        LEFT JOIN public.attributes a ON a.id = v.attribute_id
+               WHERE t.title = 'date'
+                 AND v.id IN (SELECT v2.id
+                              FROM public."values" v2
+                                       JOIN public.attributes a_1 ON v2.attribute_id = a_1.id
+                              WHERE a_1.title = ANY (ARRAY ['начало продаж', 'начало рекламы'])))
+SELECT f.title AS film,
+       d.title AS action,
+       d.value AS date
+FROM dates d
+         JOIN public.films f ON f.id = d.film_id
+WHERE d.value >= CURRENT_DATE
+  AND d.value <= (CURRENT_DATE + 20);
 
 
-ALTER TABLE public.workd OWNER TO "AK";
+ALTER TABLE public.workd
+    OWNER TO "AK";
 
 --
 -- Data for Name: attributes; Type: TABLE DATA; Schema: public; Owner: AK
@@ -266,21 +353,37 @@ COPY public.films (title, id) FROM stdin;
 -- Data for Name: values; Type: TABLE DATA; Schema: public; Owner: AK
 --
 
-INSERT INTO public.values ( type_id, attribute_id, film_id, value_text) VALUES ( 5, 7, 1, 'По версии IMDb Считается одним из величайших фильмов в истории кино');
-INSERT INTO public.values ( type_id, attribute_id, film_id, value_text) VALUES ( 5, 1, 1, 'Критики и зрители отмечают сильный сценарий, режиссуру и актерскую игру');
-INSERT INTO public.values ( type_id, attribute_id, film_id, value_boolean) VALUES ( 3, 8, 2, true);
-INSERT INTO public.values ( type_id, attribute_id, film_id, value_boolean) VALUES ( 3, 8, 1, false);
-INSERT INTO public.values ( type_id, attribute_id, film_id, value_boolean) VALUES ( 3, 9, 1, false);
-INSERT INTO public.values ( type_id, attribute_id, film_id, value_boolean) VALUES ( 3, 9, 2, true);
-INSERT INTO public.values ( type_id, attribute_id, film_id, value_text) VALUES ( 5, 1, 2, 'Считается шедевром мирового кинематографа. Хвалят режиссуру Фрэнсиса Форда Копполы и актерскую игру, особенно Марлона Брандо');
-INSERT INTO public.values ( type_id, attribute_id, film_id, value_date) VALUES ( 6, 4, 2, '1972-05-14');
-INSERT INTO public.values ( type_id, attribute_id, film_id, value_date) VALUES ( 6, 6, 1, '2025-12-16');
-INSERT INTO public.values ( type_id, attribute_id, film_id, value_date) VALUES ( 6, 4, 1, '1994-07-23');
-INSERT INTO public.values ( type_id, attribute_id, film_id, value_date) VALUES ( 6, 3, 2, '1972-04-24');
-INSERT INTO public.values ( type_id, attribute_id, film_id, value_date) VALUES ( 6, 3, 1, '1994-07-23');
-INSERT INTO public.values ( type_id, attribute_id, film_id, value_date) VALUES ( 6, 5, 1, '2025-12-15');
-INSERT INTO public.values ( type_id, attribute_id, film_id, value_date) VALUES ( 6, 6, 2, '2025-12-21');
-INSERT INTO public.values ( type_id, attribute_id, film_id, value_date) VALUES ( 6, 5, 2, '2025-12-22');
+INSERT INTO public.values (type_id, attribute_id, film_id, value_text)
+VALUES (5, 7, 1, 'По версии IMDb Считается одним из величайших фильмов в истории кино');
+INSERT INTO public.values (type_id, attribute_id, film_id, value_text)
+VALUES (5, 1, 1, 'Критики и зрители отмечают сильный сценарий, режиссуру и актерскую игру');
+INSERT INTO public.values (type_id, attribute_id, film_id, value_boolean)
+VALUES (3, 8, 2, true);
+INSERT INTO public.values (type_id, attribute_id, film_id, value_boolean)
+VALUES (3, 8, 1, false);
+INSERT INTO public.values (type_id, attribute_id, film_id, value_boolean)
+VALUES (3, 9, 1, false);
+INSERT INTO public.values (type_id, attribute_id, film_id, value_boolean)
+VALUES (3, 9, 2, true);
+INSERT INTO public.values (type_id, attribute_id, film_id, value_text)
+VALUES (5, 1, 2,
+        'Считается шедевром мирового кинематографа. Хвалят режиссуру Фрэнсиса Форда Копполы и актерскую игру, особенно Марлона Брандо');
+INSERT INTO public.values (type_id, attribute_id, film_id, value_date)
+VALUES (6, 4, 2, '1972-05-14');
+INSERT INTO public.values (type_id, attribute_id, film_id, value_date)
+VALUES (6, 6, 1, '2025-12-16');
+INSERT INTO public.values (type_id, attribute_id, film_id, value_date)
+VALUES (6, 4, 1, '1994-07-23');
+INSERT INTO public.values (type_id, attribute_id, film_id, value_date)
+VALUES (6, 3, 2, '1972-04-24');
+INSERT INTO public.values (type_id, attribute_id, film_id, value_date)
+VALUES (6, 3, 1, '1994-07-23');
+INSERT INTO public.values (type_id, attribute_id, film_id, value_date)
+VALUES (6, 5, 1, '2025-12-15');
+INSERT INTO public.values (type_id, attribute_id, film_id, value_date)
+VALUES (6, 6, 2, '2025-12-21');
+INSERT INTO public.values (type_id, attribute_id, film_id, value_date)
+VALUES (6, 5, 2, '2025-12-22');
 
 
 --
@@ -340,7 +443,7 @@ ALTER TABLE ONLY public.films
 --
 
 ALTER TABLE ONLY public."values"
-    ADD CONSTRAINT attributes___fk FOREIGN KEY (attribute_id) REFERENCES public.attributes(id);
+    ADD CONSTRAINT attributes___fk FOREIGN KEY (attribute_id) REFERENCES public.attributes (id);
 
 
 --
@@ -348,7 +451,7 @@ ALTER TABLE ONLY public."values"
 --
 
 ALTER TABLE ONLY public."values"
-    ADD CONSTRAINT films___fk FOREIGN KEY (film_id) REFERENCES public.films(id) ON UPDATE CASCADE ON DELETE CASCADE;
+    ADD CONSTRAINT films___fk FOREIGN KEY (film_id) REFERENCES public.films (id) ON UPDATE CASCADE ON DELETE CASCADE;
 
 
 --
@@ -356,7 +459,7 @@ ALTER TABLE ONLY public."values"
 --
 
 ALTER TABLE ONLY public."values"
-    ADD CONSTRAINT types___fk FOREIGN KEY (type_id) REFERENCES public.attributes_types(id);
+    ADD CONSTRAINT types___fk FOREIGN KEY (type_id) REFERENCES public.attributes_types (id);
 
 
 --
