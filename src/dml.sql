e351463868a66277edf94c0f807e3ed011f8bb20
Maksim Skoropiscev <skoro11@bk.ru>
2025-12-18 08:13:02 +0300

    fix: Changed movie costs

--- src/dml.sql
+++ src/dml.sql
@@ -30,7 +30,7 @@ insert into attribute (name, typeId) values
     ('Оскар', 6);
 
 -- Крестный отец
-insert into value (movieId, attributeId, moneyVal) values (1,  1, 1546456.43);
+insert into value (movieId, attributeId, moneyVal) values (1,  1, 7657845.53);
 insert into value (movieId, attributeId, textVal) values (1,  2, 'Смирнов Мирослав Андреевич');
 insert into value (movieId, attributeId, textVal) values (1,  3, 'Австралия');
 insert into value (movieId, attributeId, textVal) values (1,  4, 'Романтика');
@@ -41,7 +41,7 @@ insert into value (movieId, attributeId, dateVal) values (1,  8, '2025-12-18');
 insert into value (movieId, attributeId, booleanVal) values (1,  9, true);
 
 -- Титаник
-insert into value (movieId, attributeId, moneyVal) values (2,  1, 1546456.43);
+insert into value (movieId, attributeId, moneyVal) values (2,  1, 3453426.43);
 insert into value (movieId, attributeId, textVal) values (2,  2, 'Смирнов Мирослав Андреевич');
 insert into value (movieId, attributeId, textVal) values (2,  3, 'Австралия');
 insert into value (movieId, attributeId, textVal) values (2,  4, 'Комедия');
@@ -52,7 +52,7 @@ insert into value (movieId, attributeId, dateVal) values (2,  8, '2025-12-19');
 insert into value (movieId, attributeId, booleanVal) values (2,  9, true);
 
 -- Начало
-insert into value (movieId, attributeId, moneyVal) values (3,  1, 1546456.43);
+insert into value (movieId, attributeId, moneyVal) values (3,  1, 4798432.35);
 insert into value (movieId, attributeId, textVal) values (3,  2, 'Фокин Фёдор Адамович');
 insert into value (movieId, attributeId, textVal) values (3,  3, 'Ирак');
 insert into value (movieId, attributeId, textVal) values (3,  4, 'Исторический');
@@ -63,7 +63,7 @@ insert into value (movieId, attributeId, dateVal) values (3,  8, '2025-12-20');
 insert into value (movieId, attributeId, booleanVal) values (3,  9, true);
 
 -- Матрица
-insert into value (movieId, attributeId, moneyVal) values (4,  1, 1546456.43);
+insert into value (movieId, attributeId, moneyVal) values (4,  1, 7854889.40);
 insert into value (movieId, attributeId, textVal) values (4,  2, 'Фокин Фёдор Адамович');
 insert into value (movieId, attributeId, textVal) values (4,  3, 'Ирак');
 insert into value (movieId, attributeId, textVal) values (4,  4, 'Исторический');
@@ -74,7 +74,7 @@ insert into value (movieId, attributeId, dateVal) values (4,  8, '2026-01-07');
 insert into value (movieId, attributeId, booleanVal) values (4,  9, true);
 
 -- Интерстеллар
-insert into value (movieId, attributeId, moneyVal) values (5,  1, 1546456.43);
+insert into value (movieId, attributeId, moneyVal) values (5,  1, 78546456.46);
 insert into value (movieId, attributeId, textVal) values (5,  2, 'Лапшина София Кирилловна');
 insert into value (movieId, attributeId, textVal) values (5,  3, 'Австралия');
 insert into value (movieId, attributeId, textVal) values (5,  4, 'Фантастика');
@@ -96,7 +96,7 @@ insert into value (movieId, attributeId, dateVal) values (6,  8, '2026-01-09');
 insert into value (movieId, attributeId, booleanVal) values (6,  9, false);
 
 -- Побег из Шоушенка
-insert into value (movieId, attributeId, moneyVal) values (7,  1, 1546456.43);
+insert into value (movieId, attributeId, moneyVal) values (7,  1, 154213556.43);
 insert into value (movieId, attributeId, textVal) values (7,  2, 'Смирнов Мирослав Андреевич');
 insert into value (movieId, attributeId, textVal) values (7,  3, 'Корея');
 insert into value (movieId, attributeId, textVal) values (7,  4, 'Фантастика');
@@ -107,7 +107,7 @@ insert into value (movieId, attributeId, dateVal) values (7,  8, '2026-01-18');
 insert into value (movieId, attributeId, booleanVal) values (7,  9, false);
 
 -- Список Шиндлера
-insert into value (movieId, attributeId, moneyVal) values (8,  1, 1546456.43);
+insert into value (movieId, attributeId, moneyVal) values (8,  1, 1546453456.43);
 insert into value (movieId, attributeId, textVal) values (8,  2, 'Вишневская Алина Семёновна');
 insert into value (movieId, attributeId, textVal) values (8,  3, 'Корея');
 insert into value (movieId, attributeId, textVal) values (8,  4, 'Приключение');
@@ -118,7 +118,7 @@ insert into value (movieId, attributeId, dateVal) values (8,  8, '2025-12-28');
 insert into value (movieId, attributeId, booleanVal) values (8,  9, false);
 
 -- Криминальное чтиво
-insert into value (movieId, attributeId, moneyVal) values (19,  1, 1546456.43);
+insert into value (movieId, attributeId, moneyVal) values (19,  1, 1354546456.77);
 insert into value (movieId, attributeId, textVal) values (19,  2, 'Вишневская Алина Семёновна');
 insert into value (movieId, attributeId, textVal) values (19,  3, 'Лаос');
 insert into value (movieId, attributeId, textVal) values (19,  4, 'Комедия');
@@ -129,7 +129,7 @@ insert into value (movieId, attributeId, dateVal) values (19,  8, '2025-12-19');
 insert into value (movieId, attributeId, booleanVal) values (19,  9, false);
 
 -- Трон: Наследие
-insert into value (movieId, attributeId, moneyVal) values (10,  1, 1546456.43);
+insert into value (movieId, attributeId, moneyVal) values (10,  1, 100876456.73);
 insert into value (movieId, attributeId, textVal) values (10,  2, 'Кузнецов Максим Максимович');
 insert into value (movieId, attributeId, textVal) values (10,  3, 'Лаос');
 insert into value (movieId, attributeId, textVal) values (10,  4, 'Приключение');
