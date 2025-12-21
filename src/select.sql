08fd8d90eb3d1826058dadb022483e2a960816c5
Maksim Skoropiscev <skoro11@bk.ru>
2025-12-21 13:24:19 +0300

    feat: Second request from task | Received  cost and director

--- src/select.sql
+++ src/select.sql
@@ -12,3 +12,25 @@ from movie as m
          left join premiere_is_today as t on m.id = t.movieId
          left join premiere_is_in_twenty_days as t2 on m.id = t2.movieId
 where t2.dateVal is not null or t.dateVal is not null
+
+-- Фильм, тип атрибута, атрибут, значение
+
+with cost as (
+	select movieId, 'Стоимость фильма в $' as attributeName, v.attributeId, CAST(moneyVal as text) as value from value as v
+	where v.attributeId = (select id from attribute where name = 'Стоимость фильма в $')
+),
+director as (
+	select movieId, 'Режиссёр' as attributeName, v.attributeId, textVal as value from value as v
+	where v.attributeId = (select id from attribute where name = 'Режиссёр')
+),
+result as (
+	select * from cost
+	union all
+	select * from director
+)
+
+select m.name, r.attributeName, t.name, r.value from result as r
+left join movie as m on m.id = r.movieId
+left join attribute_type as t on r.attributeId= t.id
+
+group by m.name, attributename, t.name, r.value
