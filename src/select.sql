0ab1f9b1ca99b2f86cdee704733bdfb4b7b14029
Maksim Skoropiscev <skoro11@bk.ru>
2025-12-21 17:09:19 +0300

    feat: Second request from task | Received genre, duration, rating, year_of_release, premiere_date, oscar

--- src/select.sql
+++ src/select.sql
@@ -27,12 +27,49 @@ country as (
 	select movieId, 'Страна' as attributeName, v.attributeId, textVal as value from value as v
 	where v.attributeId = (select id from attribute where name = 'Страна')
 ),
+genre as (
+	select movieId, 'Жанр' as attributeName, v.attributeId, textVal as value from value as v
+	where v.attributeId = (select id from attribute where name = 'Жанр')
+),
+duration as (
+	select movieId, 'Длительность в минутах' as attributeName, v.attributeId, CAST(integerVal as text) as value from value as v
+	where v.attributeId = (select id from attribute where name = 'Длительность в минутах')
+),
+rating as (
+	select movieId, 'Рейтинг' as attributeName, v.attributeId, CAST(realVal as text) as value from value as v
+	where v.attributeId = (select id from attribute where name = 'Рейтинг')
+),
+year_of_release as (
+	select movieId, 'Год выпуска' as attributeName, v.attributeId, CAST(integerVal as text) as value from value as v
+	where v.attributeId = (select id from attribute where name = 'Год выпуска')
+),
+premiere_date as (
+	select movieId, 'Дата премьеры' as attributeName, v.attributeId, CAST(dateVal as text) as value from value as v
+	where v.attributeId = (select id from attribute where name = 'Дата премьеры')
+),
+oscar as (
+	select movieId, 'Оскар' as attributeName, v.attributeId, CAST(booleanVal as text) as value from value as v
+	where v.attributeId = (select id from attribute where name = 'Оскар')
+),
+
 result as (
 	select * from cost
 	union all
 	select * from director
 	union all
 	select * from country
+	union all
+	select * from genre
+	union all
+	select * from duration
+	union all
+	select * from rating
+	union all
+	select * from year_of_release
+	union all
+	select * from premiere_date
+	union all
+	select * from oscar
 )
 
 select m.name, r.attributeName, t.name, r.value from result as r
