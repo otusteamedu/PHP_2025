cb1cf06c9b51182a4bb7a224c8b545b2e6cee67f
Maksim Skoropiscev <skoro11@bk.ru>
2025-12-29 22:00:37 +0300

    feat: Created movie table

--- src/ddl.sql
+++ src/ddl.sql
@@ -1,4 +1,4 @@
--- drop table cinema.customer, cinema.cinema;
+-- drop table cinema.customer, cinema.cinema, cinema.movie;
 create schema cinema;
 
 create table cinema.customer
@@ -14,4 +14,10 @@ create table cinema.cinema
     id smallserial primary key,
     city character varying(50) not null,
     address text not null
+);
+
+create table cinema.movie
+(
+    id bigserial primary key,
+    name character varying(100) not null
 );
