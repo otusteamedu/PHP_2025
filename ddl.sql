a97c7a686a33c79035f0d1c2e07ca3031270b0be
arlex2305k <alex@digsol.ru>
2026-01-16 10:35:50 +0300

    HW#13

--- ddl.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE MOVIE (
-    id SERIAL PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
-    description TEXT,
-    duration_minutes INT NOT NULL,
-    genre VARCHAR(100),
-    rating DECIMAL(3,1)
-);
