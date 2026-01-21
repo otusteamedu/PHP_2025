e5539ce68817390434d4f5bf0d285c27b802ab06
Alex <69921553+AlexGenious48@users.noreply.github.com>
2026-01-22 00:08:15 +0300

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,11 +1,9 @@
 <?php
 
 require_once __DIR__ . '/../vendor/autoload.php';
-use App\Database\Database;
-use App\Controller\BotController;
-use App\Controller\UserApiController;
-
-use App\Service\TelegramBot;
+use App\Application\ApplicationRouter;
+$method = $_SERVER['REQUEST_METHOD'];
+$path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $envFile = __DIR__ . '/../.env';
 if (file_exists($envFile)) {
     $lines = file($envFile, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
@@ -16,31 +14,4 @@ if (file_exists($envFile)) {
         $_ENV[$name] = $value;
     }
 }
-$botToken = $_ENV['BOT_TOKEN'] ?? getenv('BOT_TOKEN');
-$bitrixUrl = $_ENV['BITRIX_URL'] ?? getenv('BITRIX_URL');
-// // Определяем хост
-
-$host = $_SERVER['HTTP_HOST'] ?? ($_SERVER['SERVER_NAME'] ?? 'localhost');
-$webhookUrl = $protocol . '://' . $host;
-// Создаем экземпляры
-$bot = new TelegramBot($botToken, $webhookUrl, $bitrixUrl);
-$controller = new BotController($bot);
-
-$response = $controller->checkPermissions();
-if ($response) {
-    $currState = $controller->getCurrentState();
-    if ( $currState === null ) {
-        $controller->setCurrentUserStartState();
-        $controller->sendStartMessage();
-    } else if ($currState === "start") {
-        $controller->checkDealsRequest();
-    } else if ($currState === "deals") {
-        $result = $controller->checkDealOptionCallbackRequest();
-        if ($result != null) {
-            $controller->setCurrentUserChangeCountDealState($result);
-        }
-    } else if (str_contains($currState , "change_count_deal")) {
-        $controller->checkChangeCountDealRequest($currState);
-    }
-    
-}
+ApplicationRouter::init($method, $path, $_ENV);
