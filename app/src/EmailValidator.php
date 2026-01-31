0d026ba409fb0320ec4bfc0593483e91b812acb2
Maksim Skoropiscev <skoro11@bk.ru>
2026-01-31 11:54:20 +0300

    feat: Email validation has been completed

--- app/src/EmailValidator.php
+++ app/src/EmailValidator.php
@@ -1,5 +1,10 @@
 <?php
 namespace Pryaniki\App;
+
+use Pryaniki\App\Exceptions;
+use Pryaniki\App\Exceptions\EmptyFieldException;
+use Pryaniki\App\Exceptions\NotValidException;
+
 class EmailValidator
 {
     public static function validate(string $email): bool
@@ -11,15 +16,25 @@ class EmailValidator
             self::checkDns($email);
             $isValid = true;
         } catch (\Exception $e) {
-
+            echo $e->getMessage() . PHP_EOL;
         }
 
         return $isValid;
     }
 
+    /**
+     * @throws NotValidException
+     * @throws EmptyFieldException
+     */
     private static function checkEmail(string $email): void
     {
+        if ($email === '') {
+            throw new Exceptions\EmptyFieldException('email');
+        }
 
+        if (filter_var($email, FILTER_VALIDATE_EMAIL) === false) {
+            throw new Exceptions\NotValidException('email');
+        }
     }
     private static function checkDns(string $email): void
     {
