43a81f8f80f78fe46cb28fefcb5433d6cff62b13
AK <hooploop14@gmail.com>
2026-02-02 19:59:38 +0300

    Вызов нашего приложения
    маршрутизация на основе GET-параметров

--- code/index.php
+++ code/index.php
@@ -1,2 +1,35 @@
 <?php
-require __DIR__ . '/vendor/autoload.php';
+define('BASE_PATH', __DIR__);
+require BASE_PATH . '/vendor/autoload.php';
+
+use App\Container;
+
+$container = new Container();
+
+// маршрутизация на основе GET-параметров
+
+$controllerName = $_GET['controller'] ?? 'index';
+$actionName = $_GET['action'] ?? 'index';
+
+$controllerClass = 'App\\Controllers\\' . $controllerName . 'Controller';
+try {
+    if (!$container->has($controllerClass)) {
+        throw new Exception("Controller '{$controllerClass}' is not registered in the container.");
+    }
+
+    $controller = $container->get($controllerClass);
+
+} catch (Exception $e) {
+    header("HTTP/1.1 500 Internal Server Error");
+    exit;
+}
+
+$actionMethod = $actionName . 'Action';
+
+if (!method_exists($controller, $actionMethod)) {
+    header("HTTP/1.1 404 Not Found");
+    die("Action '{$actionMethod}' not found in controller '{$controllerClass}'.");
+}
+
+$params = array_values(array_diff_key($_GET, ['controller' => '', 'action' => '']));
+call_user_func_array([$controller, $actionMethod], $params);
