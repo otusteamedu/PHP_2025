911aae60dce5a82ae6811c65c13e9a63adead166
AK <hooploop14@gmail.com>
2026-02-04 22:00:03 +0300

    Удалил файлы старой реализации

--- code/src/App/config/Constants.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-define('APP_BASE_URL', str_replace('index.php','',$_SERVER['SCRIPT_NAME']));
-define('APP_BASE_PATH',realpath(__DIR__. DIRECTORY_SEPARATOR.'../../'));
-define('APP_DEFAULT_CONTROLLER', 'index');
