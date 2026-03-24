516b070aeaaf2b16c83743c0413209a79e0a3b5d
Maksim Skoropiscev <skoro11@bk.ru>
2026-03-24 20:17:27 +0300

    feat(tasks): Completed task 2

--- src/tasks.sql
+++ src/tasks.sql
@@ -5,6 +5,10 @@ join cinema.movie as m on m.id = s.movie_id
 where s.start_time >= current_date and  s.start_time < current_date + interval '1 day';
 
 --2. Подсчёт проданных билетов за неделю
+select count(t.id)
+from cinema.ticket as t
+join cinema.orders as o on o.id = t.order_id
+where o.created_at >= date_trunc('week', now()) and  o.created_at < date_trunc('week', now()) + interval '1 week'
 
 --3. Формирование афиши (фильмы, которые показывают сегодня)
 
