760f4d463b6a92f6aabca4c338638512dc0c0cff
Maksim Skoropiscev <skoro11@bk.ru>
2026-04-08 07:28:27 +0300

    feat: Created AppRunner

---
+++ app/src/Presentation/AppRunner.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Presentation;
+
+class AppRunner
+{
+    public function prepareOrder(array $arOrder): void
+    {
+        // todo - usecase по созданию заказа
+        // todo заказ передается на кухню и возвращается блюда + сообщения об ошибках (нехватка ингредиентов)
+    }
+}
