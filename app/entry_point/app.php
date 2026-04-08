424cef9b5f40e5854b040a0f186ed6ab9fec02f5
Maksim Skoropiscev <skoro11@bk.ru>
2026-04-08 07:30:10 +0300

    feat: Created entry_point

---
+++ app/entry_point/app.php
@@ -0,0 +1,46 @@
+<?php
+
+use App\Presentation\AppRunner;
+
+$order = [
+    'burger' =>  [
+        [
+            'type' => 'classic',
+            'count' => 1,
+            'additional_ingredients' => [
+                'onion' => 1
+            ]
+        ]
+    ],
+    'pizza' => [
+        [
+            'type' => 'classic',
+            'count' => 1,
+            'additional_ingredients' => [
+                'cheese' => 1
+            ]
+        ]
+    ],
+    'hot-dog' => [
+        [
+            'type' => 'classic',
+            'count' => 2,
+            'additional_ingredients' => [
+                'fried-onions' => 1
+            ]
+        ]
+    ],
+    'sandwich' => [
+        [
+            'type' => 'classic',
+            'count' => 1,
+            'additional_ingredients' => [
+                'fried-onions' => 1
+            ]
+        ]
+    ],
+];
+
+$app = new AppRunner();
+
+$app->prepareOrder($order);
