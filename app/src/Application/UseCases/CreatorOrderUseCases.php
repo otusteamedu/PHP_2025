1b150871971b0db22ffdbcb55a04f80b5cb7ef25
Maksim Skoropiscev <skoro11@bk.ru>
2026-04-12 17:31:08 +0300

    feat: CreatorOrderUseCases was created

--- app/src/Application/UseCases/CreatorOrderUseCases.php
+++ app/src/Application/UseCases/CreatorOrderUseCases.php
@@ -2,10 +2,43 @@
 
 namespace App\Application\UseCases;
 
+use App\Domain\Ingredient\IngredientInterface;
+use App\Domain\Order\Order;
+use App\Domain\Order\OrderInterface;
+use App\Domain\Order\OrderItemModel;
+use App\Infrastructure\Ingredient\IngredientFactory;
+use App\Infrastructure\Ingredient\IngredientLoader;
+
 class CreatorOrderUseCases
 {
-    public function getOrder(array $arOrder)
+    public function execute(array $arOrderConfig): OrderInterface
     {
+        $order = new Order();
 
+        foreach ($arOrderConfig as $productName => $arSettings) {
+            foreach ($arSettings as $arProductSetting) {
+                $additionalIngredients = $arProductSetting['additional_ingredients']
+                    ? $this->getAdditionalIngredients($arProductSetting['additional_ingredients'])
+                    : [];
+                $orderItemModel = new OrderItemModel(
+                    productName: $productName,
+                    recipeType: $arProductSetting['type'],
+                    count: $arProductSetting['count'],
+                    additionalIngredients: $additionalIngredients
+                );
+                $order->addItem($orderItemModel);
+            }
+        }
+
+        return $order;
+    }
+
+    /** @return IngredientInterface[] */
+    private function getAdditionalIngredients(array $arIngredients): array
+    {
+        $ingredientsFactory = new IngredientFactory();
+        $ingredientLoader = new IngredientLoader($ingredientsFactory);
+
+        return $ingredientLoader->loadFromArray($arIngredients);
     }
 }
