3d0d7ffd8263ddd4ebc447d8f7bbff21c5303bad
Maksim Skoropiscev <skoro11@bk.ru>
2026-04-13 07:42:23 +0300

    feat: BurgerStrategy class was created

---
+++ app/src/Domain/Product/Strategy/BurgerStrategy.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Domain\Product\Strategy;
+
+use App\Domain\Product\Product;
+
+class BurgerStrategy implements ProductStrategyInterface
+{
+
+    public function create(): Product
+    {
+        return new Product('burger');
+    }
+
+    public function getBaseIngredients(string $type): array
+    {
+        return match ($type) {
+           'classic' => [
+               'bun' => 2,
+               'cutlet' => 1,
+               'ketchup' => 1,
+            ],
+            default => throw new \RuntimeException("Unknown burger type")
+        };
+    }
+}
