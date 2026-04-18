e427c1fb5cf56cb7add0a35abc9d06afdd4eefdc
AK <hooploop14@gmail.com>
2026-04-18 20:30:08 +0300

    классы Repository

---
+++ code/src/Repository/UserRepositoryInterface.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Repository;
+
+use App\Domain\Entity\User;
+
+interface UserRepositoryInterface extends RepositoryInterface
+{
+    /**
+     * @param int $id
+     * @return User|null
+     */
+    public function findById(int $id): ?object;
+
+    /**
+     * @param string $email
+     * @return User|null
+     */
+    public function findByEmail(string $email): ?User;
+
+    /**
+     * @param int $page
+     * @param int $limit
+     * @return User[]
+     */
+    public function findAll(int $page = 1, int $limit = 10): array;
+
+    /**
+     * @param User $entity
+     * @return User
+     */
+    public function save(object $entity): object;
+
+    /**
+     * @param User $entity
+     */
+    public function remove(object $entity): void;
+}
