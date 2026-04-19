720d8097248e16b355676b0bef43d5b10a4b867a
Maksim Skoropiscev <skoro11@bk.ru>
2026-04-19 19:01:44 +0300

    feat: ProductBuilder | lass constructor has been created and the build method was added

--- app/src/Domain/Product/Builder/ProductBuilder.php
+++ app/src/Domain/Product/Builder/ProductBuilder.php
@@ -2,13 +2,28 @@
 
 namespace App\Domain\Product\Builder;
 
+use App\Domain\Ingredient\Storage\IngredientStorageInterface;
+use App\Domain\Product\Chain\CookingContext;
+use App\Domain\Product\Chain\CookingHandlerInterface;
 use App\Domain\Product\ProductInterface;
 
 class ProductBuilder implements ProductBuilderInterface
 {
+    public function __construct(
+        private CookingHandlerInterface $chain,
+        private IngredientStorageInterface $storage
+    ) {}
     public function build(ProductInterface $product, array $baseIngredients, array $additionalIngredients): ProductInterface
     {
-        //todo реализовать процесс готовки
+        $context = new CookingContext(
+            $product,
+            $baseIngredients,
+            $additionalIngredients,
+            $this->storage
+        );
+
+        $this->chain->handle($context);
+
         return $product;
     }
 }
