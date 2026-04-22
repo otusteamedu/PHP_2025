4b8e0fb24f384bad3401545cc2ee67e441fa25d5
AK <hooploop14@gmail.com>
2026-04-22 18:32:38 +0300

    Интерфейсы для Training  \ UserTraining

---
+++ code/src/Repository/TrainingPlanRepositoryInterface.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Repository;
+
+use App\Domain\Entity\TrainingPlan;
+
+interface TrainingPlanRepositoryInterface extends RepositoryInterface
+{
+    /**
+     * @param int $id
+     * @return TrainingPlan|null
+     */
+    public function findById(int $id): ?object;
+
+    /**
+     * @param int $userId
+     * @return TrainingPlan[]
+     */
+    public function findByUserId(int $userId): array;
+
+    /**
+     * @param int $page
+     * @param int $limit
+     * @return TrainingPlan[]
+     */
+    public function findAll(int $page = 1, int $limit = 10): array;
+
+    /**
+     * @param TrainingPlan $entity
+     * @return TrainingPlan
+     */
+    public function save(object $entity): object;
+
+    /**
+     * @param TrainingPlan $entity
+     */
+    public function remove(object $entity): void;
+}
---
+++ code/src/Repository/UserTrainingPlanRepositoryInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Repository;
+
+interface UserTrainingPlanRepositoryInterface
+{
+    public function assign(int $userId, int $trainingPlanId): void;
+    public function unassign(int $userId, int $trainingPlanId): void;
+    public function isAssigned(int $userId, int $trainingPlanId): bool;
+}
