d993e7d7589a87b95a18756ecb70cc2122bd6c01
AK <hooploop14@gmail.com>
2026-04-22 18:41:07 +0300

    Контроллер Training Plan

---
+++ code/src/Presentation/Controller/TrainingPlan/TrainingPlanController.php
@@ -0,0 +1,69 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Presentation\Controller\TrainingPlan;
+
+use App\Application\UseCase;
+use App\Presentation\Validation\ValidationException;
+use App\Presentation\Validation\TrainingPlanValidator;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+/**
+ * Контроллер для управления планами тренировок.
+ */
+readonly class TrainingPlanController
+{
+    /**
+     * @param TrainingPlanValidator $validator Валидатор данных плана тренировок.
+     * @param UseCase\CreateTrainingPlanUseCase $createTrainingPlanUseCase Use case для создания плана тренировок.
+     */
+    public function __construct(
+        private TrainingPlanValidator $validator,
+        private UseCase\CreateTrainingPlanUseCase $createTrainingPlanUseCase,
+    ) {
+    }
+
+    /**
+     * Создает новый план тренировок.
+     *
+     * @param Request $request PSR-7 запрос.
+     * @param Response $response PSR-7 ответ.
+     * @return Response Ответ с результатом операции.
+     * @throws \JsonException
+     */
+    public function createTrainingPlan(Request $request, Response $response): Response
+    {
+        $data = (array)$request->getParsedBody();
+
+        try {
+            $this->validator->validate($data);
+        } catch (ValidationException $exception) {
+            $response->getBody()->write(json_encode(['errors' => $exception->getErrors()], JSON_THROW_ON_ERROR | JSON_PRETTY_PRINT));
+            return $response->withHeader('Content-Type', 'application/json')->withStatus(400);
+        }
+
+        try {
+            $trainingPlan = $this->createTrainingPlanUseCase->execute($data);
+        } catch (\Exception $exception) {
+            $response->getBody()->write(json_encode(['error' => $exception->getMessage()], JSON_THROW_ON_ERROR | JSON_PRETTY_PRINT));
+            return $response->withHeader('Content-Type', 'application/json')->withStatus(500);
+        }
+
+        $responseData = [
+            'message' => 'Training plan created successfully.',
+            'training_plan' => [
+                'id' => $trainingPlan->getId(),
+                'user_id' => $trainingPlan->getUserId(),
+                'name' => $trainingPlan->getName(),
+                'description' => $trainingPlan->getDescription(),
+                'created_at' => $trainingPlan->getCreatedAt()->format('Y-m-d H:i:s'),
+            ]
+        ];
+
+        $response->getBody()->write(json_encode($responseData, JSON_THROW_ON_ERROR | JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES));
+
+        return $response->withHeader('Content-Type', 'application/json')->withStatus(201);
+    }
+}
