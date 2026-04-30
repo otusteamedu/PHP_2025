7a63a21f79420bcc36779007112ce1548d9f9fa1
Maksim Skoropiscev <skoro11@bk.ru>
2026-04-30 07:33:55 +0300

    feat: Margins changed

--- src/selects-for-task.sql
+++ src/selects-for-task.sql
@@ -18,7 +18,7 @@ where s.id = 3258
 -- Сложные запросы
 
 -- 1
-                            explain with sessions as (
+explain with sessions as (
     select s.id as id, m.name as movie_name
     from cinema.session as s
              join cinema.movie as m on m.id = s.movie_id
@@ -29,14 +29,14 @@ select
     s.movie_name,
     sum(t.price) as total_revenue
 from sessions as s
-         join cinema.ticket as t on s.id = t.session_id
+     join cinema.ticket as t on s.id = t.session_id
 group by s.movie_name
 order by total_revenue desc
 
 -- 2
 explain select count(t.id)
 from cinema.ticket as t
-         join cinema.orders as o on o.id = t.order_id
+    join cinema.orders as o on o.id = t.order_id
 where o.created_at >= date_trunc('week', now()) and  o.created_at < date_trunc('week', now()) + interval '1 week'
 
 
@@ -45,23 +45,23 @@ explain with hall as (
     select hall_id as id from cinema.session as s where s.id = 3258
     ),
     places as (
-    select row, seat, p.id
-    from cinema.place as p
-    join hall as h on p.hall_id = h.id
+        select row, seat, p.id
+        from cinema.place as p
+        join hall as h on p.hall_id = h.id
     ),
     busy_places as (
-    select t.place_id
-    from cinema.ticket as t
-    where t.session_id = 3258
+        select t.place_id
+        from cinema.ticket as t
+        where t.session_id = 3258
     ),
     seats as (
     select
-    p.row,
-    p.seat,
-    case
-    when b.place_id is null then 'o'
-    else 'x'
-    end as status
+        p.row,
+        p.seat,
+        case
+        when b.place_id is null then 'o'
+        else 'x'
+        end as status
     from places p
     left join busy_places b on b.place_id = p.id
     )
