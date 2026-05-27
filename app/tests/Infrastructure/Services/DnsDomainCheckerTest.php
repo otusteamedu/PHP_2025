afe314322244be16141afb76c6a785f5a57f1a03
Maksim Skoropiscev <skoro11@bk.ru>
2026-05-27 19:08:27 +0300

    test(Infrastructure): added tests for DnsDomainChecker

---
+++ app/tests/Infrastructure/Services/DnsDomainCheckerTest.php
@@ -0,0 +1,46 @@
+<?php
+namespace Pryaniki\App\Infrastructure\Services;
+
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\TestCase;
+
+class DnsDomainCheckerTest extends TestCase
+{
+    #[DataProvider('validDomainDataProvider')]
+    public function testValidDomain(string $domain): void
+    {
+        self::assertTrue(
+            DnsDomainChecker::isExistsDomain($domain),
+            sprintf('Domain "%s" does not exist', $domain),
+        );
+    }
+
+    public static function validDomainDataProvider(): array
+    {
+        return [
+            ['yandex.ru'],
+            ['ya.ru'],
+            ['yandx.ru'],
+            ['gmail.com'],
+        ];
+    }
+
+    #[DataProvider('invalidDomainDataProvider')]
+    public function testInvalidDomain(string $domain): void
+    {
+        self::assertFalse(
+            DnsDomainChecker::isExistsDomain($domain),
+            sprintf('Domain "%s" exist', $domain),
+        );
+    }
+
+    public static function invalidDomainDataProvider(): array
+    {
+        return [
+            ['gmal.com'],
+            ['yan1dx.ru'],
+            ['test@test.ru'],
+            [''],
+        ];
+    }
+}
