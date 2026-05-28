52f1d39627604b8310e3f4fb1bf18d3b9189e9a6
AK <hooploop14@gmail.com>
2026-05-28 06:35:39 +0300

    Исправил обработку Exception - добавил ожидаемые типы исключений

--- code/src/Presentation/Controller/TrainingPlan/UserController.php
+++ code/src/Presentation/Controller/TrainingPlan/UserController.php
@@ -3,9 +3,10 @@ declare(strict_types=1);
 namespace App\Presentation\Controller\TrainingPlan;
 
 use App\Application\UseCase\TrainingPlan\AssignTrainingPlanToUserUseCase;
+use App\Domain\Exception\TrainingPlanNotFoundException;
+use App\Domain\Exception\UserNotFoundException;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
-use App\Domain\Exception\DomainRecordNotFoundException;
 
 class UserController {
     public function __construct(
@@ -23,7 +24,7 @@ class UserController {
 
             $response->getBody()->write(json_encode(['message' => 'User successfully attached to training plan']));
             return $response->withStatus(200)->withHeader('Content-Type', 'application/json');
-        } catch (DomainRecordNotFoundException $e) {
+        } catch (UserNotFoundException | TrainingPlanNotFoundException $e) {
             $response->getBody()->write(json_encode(['message' => $e->getMessage()]));
             return $response->withStatus(404)->withHeader('Content-Type', 'application/json');
         }
