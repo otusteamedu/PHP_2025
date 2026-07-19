ed699747ee7430ba2c24230379961fdda2688d86
Maksim Skoropiscev <skoro11@bk.ru>
2026-07-19 19:19:05 +0300

    feat: change TaskQueueInterface

--- app/src/Domain/Queue/TaskQueueInterface.php
+++ app/src/Domain/Queue/TaskQueueInterface.php
@@ -5,10 +5,11 @@ declare(strict_types=1);
 namespace App\Domain\Queue;
 
 use App\Domain\Entity\Task;
+use App\Domain\Handler\TaskHandlerInterface;
 
 interface TaskQueueInterface
 {
     public function push(Task $task): void;
 
-    public function pop(): ?Task;
+    public function consume(TaskHandlerInterface  $handler): void;
 }
