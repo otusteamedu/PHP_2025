3441fea7b26feaa9efcb5fa4e255724df4c897c5
Maksim Skoropiscev <skoro11@bk.ru>
2026-07-28 09:16:43 +0300

    feat: remove usage tasting variables from .env

--- app/src/Infrastructure/Database/Config/DatabaseConfigLoader.php
+++ app/src/Infrastructure/Database/Config/DatabaseConfigLoader.php
@@ -8,15 +8,13 @@ final class DatabaseConfigLoader
 {
     public static function load(): DatabaseConfig
     {
-        $isTesting = getenv('APP_ENV') === 'testing';
-
         return new DatabaseConfig(
             getenv('DB_DRIVER'),
-            $isTesting ? getenv('DB_TEST_HOST') : getenv('DB_HOST'),
-            (int) ($isTesting ? getenv('DB_TEST_PORT') : getenv('DB_PORT')),
-            $isTesting ? getenv('DB_TEST_NAME') : getenv('DB_NAME'),
-            $isTesting ? getenv('DB_TEST_USER') : getenv('DB_USER'),
-            $isTesting ? getenv('DB_TEST_PASSWORD') : getenv('DB_PASSWORD'),
+            getenv('DB_HOST'),
+            (int)getenv('DB_PORT'),
+            getenv('DB_NAME'),
+            getenv('DB_USER'),
+            getenv('DB_PASSWORD'),
         );
     }
 }
